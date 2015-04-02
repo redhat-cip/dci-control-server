f3fb516419992c51bceb4f24292625859c61f0de
Gonéri Le Bouder <goneri.lebouder@enovance.com>
2015-04-02 14:52:46 +0200

    refresh the SQL schema
    = break down the status and the jobstate table
    
    This to be able to associate files on a jobstate.
    This away, we can do:
    
    - step 1 is a success and this is the log file
    - step 2 is a success and this is the two associated files
    - step 3 is a failure and this is the error log file.
    
    = Types
    
    - Replace `date` by `timestamp with time zone`.
    - Validate the jobstate.status (new, ongoing, failed, success)
    
    And some adjustement
    
    Change-Id: I9b0799715f186fa1f944880645028bf3683e32a4

--- db_schema/dci-control-server.sql
+++ db_schema/dci-control-server.sql
@@ -10,14 +10,14 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -37,6 +37,25 @@ CREATE FUNCTION gen_uuid() RETURNS uuid
 ALTER FUNCTION public.gen_uuid() OWNER TO postgres;
 
 --
+-- Name: jobstate_status_in_list(); Type: FUNCTION; Schema: public; Owner: boa
+--
+
+CREATE FUNCTION jobstate_status_in_list() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+IF new.status IN ('new', 'ongoing', 'success', 'failure') THEN
+    RETURN NEW;
+ELSE
+    RAISE EXCEPTION 'Bad status. valid are: new, ongoing, success, failure';
+END IF;
+END;
+$$;
+
+
+ALTER FUNCTION public.jobstate_status_in_list() OWNER TO boa;
+
+--
 -- Name: refresh_update_at_column(); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
@@ -52,13 +71,13 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: environments; Type: TABLE; Schema: public; Owner: boa; Tablespace: 
+-- Name: environments; Type: TABLE; Schema: public; Owner: boa; Tablespace:
 --
 
 CREATE TABLE environments (
-    uuid uuid DEFAULT gen_uuid() NOT NULL,
-    created_at date DEFAULT now() NOT NULL,
-    updated_at date,
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
     name character varying(255) NOT NULL
 );
 
@@ -66,64 +85,63 @@ CREATE TABLE environments (
 ALTER TABLE public.environments OWNER TO boa;
 
 --
--- Name: job_environments; Type: TABLE; Schema: public; Owner: boa; Tablespace: 
+-- Name: files; Type: TABLE; Schema: public; Owner: boa; Tablespace:
 --
 
-CREATE TABLE job_environments (
-    uuid uuid DEFAULT gen_uuid() NOT NULL,
-    environment uuid,
-    job uuid,
-    create_at date DEFAULT now() NOT NULL
+CREATE TABLE files (
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
+    name character varying(512) NOT NULL,
+    content text NOT NULL,
+    mime character varying(100) DEFAULT 'text/plain'::character varying NOT NULL,
+    md5 character varying(32),
+    jobstate_id uuid NOT NULL
 );
 
 
-ALTER TABLE public.job_environments OWNER TO boa;
+ALTER TABLE public.files OWNER TO boa;
 
 --
--- Name: jobs; Type: TABLE; Schema: public; Owner: boa; Tablespace: 
+-- Name: jobs; Type: TABLE; Schema: public; Owner: boa; Tablespace:
 --
 
 CREATE TABLE jobs (
-    uuid uuid DEFAULT gen_uuid() NOT NULL,
-    created_at date DEFAULT now() NOT NULL,
-    updated_at date,
-    name character varying(255) NOT NULL,
-    description text,
-    platform uuid NOT NULL,
-    status character varying(12) DEFAULT 'NEW'::text NOT NULL,
-    content text NOT NULL,
-    finished_at date
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
+    platform_id uuid NOT NULL,
+    scenario_id uuid NOT NULL,
+    environment_id uuid NOT NULL
 );
 
 
 ALTER TABLE public.jobs OWNER TO boa;
 
 --
--- Name: logs; Type: TABLE; Schema: public; Owner: boa; Tablespace: 
+-- Name: jobstates; Type: TABLE; Schema: public; Owner: boa; Tablespace:
 --
 
-CREATE TABLE logs (
-    uuid uuid DEFAULT gen_uuid() NOT NULL,
-    created_at date DEFAULT now() NOT NULL,
-    updated_at date,
-    name text NOT NULL,
-    content bytea NOT NULL,
-    mime character varying(100) NOT NULL,
-    checksum character varying(64) NOT NULL,
-    job uuid NOT NULL
+CREATE TABLE jobstates (
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
+    status character varying,
+    comment text,
+    job_id uuid NOT NULL
 );
 
 
-ALTER TABLE public.logs OWNER TO boa;
+ALTER TABLE public.jobstates OWNER TO boa;
 
 --
--- Name: platforms; Type: TABLE; Schema: public; Owner: boa; Tablespace: 
+-- Name: platforms; Type: TABLE; Schema: public; Owner: boa; Tablespace:
 --
 
 CREATE TABLE platforms (
-    uuid uuid DEFAULT gen_uuid() NOT NULL,
-    created_at date DEFAULT now() NOT NULL,
-    updated_at date,
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
     name character varying(255)
 );
 
@@ -131,7 +149,22 @@ CREATE TABLE platforms (
 ALTER TABLE public.platforms OWNER TO boa;
 
 --
--- Name: environments_name_key; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
+-- Name: scenarios; Type: TABLE; Schema: public; Owner: boa; Tablespace:
+--
+
+CREATE TABLE scenarios (
+    id uuid DEFAULT gen_uuid() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone,
+    name character varying(255) NOT NULL,
+    content text NOT NULL
+);
+
+
+ALTER TABLE public.scenarios OWNER TO boa;
+
+--
+-- Name: environments_name_key; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
 --
 
 ALTER TABLE ONLY environments
@@ -139,51 +172,59 @@ ALTER TABLE ONLY environments
 
 
 --
--- Name: environments_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
+-- Name: environments_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
 --
 
 ALTER TABLE ONLY environments
-    ADD CONSTRAINT environments_pkey PRIMARY KEY (uuid);
+    ADD CONSTRAINT environments_pkey PRIMARY KEY (id);
 
 
 --
--- Name: job_environments_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
+-- Name: files_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
 --
 
-ALTER TABLE ONLY job_environments
-    ADD CONSTRAINT job_environments_pkey PRIMARY KEY (uuid);
-
-
---
--- Name: jobs_name_key; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
---
-
-ALTER TABLE ONLY jobs
-    ADD CONSTRAINT jobs_name_key UNIQUE (name);
+ALTER TABLE ONLY files
+    ADD CONSTRAINT files_pkey PRIMARY KEY (id);
 
 
 --
--- Name: jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
+-- Name: jobs_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
 --
 
 ALTER TABLE ONLY jobs
-    ADD CONSTRAINT jobs_pkey PRIMARY KEY (uuid);
+    ADD CONSTRAINT jobs_pkey PRIMARY KEY (id);
 
 
 --
--- Name: logs_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
---
-
-ALTER TABLE ONLY logs
-    ADD CONSTRAINT logs_pkey PRIMARY KEY (uuid);
-
-
---
--- Name: platforms_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace: 
+-- Name: platforms_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
 --
 
 ALTER TABLE ONLY platforms
-    ADD CONSTRAINT platforms_pkey PRIMARY KEY (uuid);
+    ADD CONSTRAINT platforms_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: scenarios_name_key; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
+--
+
+ALTER TABLE ONLY scenarios
+    ADD CONSTRAINT scenarios_name_key UNIQUE (name);
+
+
+--
+-- Name: scenarios_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
+--
+
+ALTER TABLE ONLY scenarios
+    ADD CONSTRAINT scenarios_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: status_pkey; Type: CONSTRAINT; Schema: public; Owner: boa; Tablespace:
+--
+
+ALTER TABLE ONLY jobstates
+    ADD CONSTRAINT status_pkey PRIMARY KEY (id);
 
 
 --
@@ -194,6 +235,13 @@ CREATE TRIGGER refresh_environments_update_at_column BEFORE UPDATE ON environmen
 
 
 --
+-- Name: refresh_files_update_at_column; Type: TRIGGER; Schema: public; Owner: boa
+--
+
+CREATE TRIGGER refresh_files_update_at_column BEFORE UPDATE ON files FOR EACH ROW EXECUTE PROCEDURE refresh_update_at_column();
+
+
+--
 -- Name: refresh_jobs_update_at_column; Type: TRIGGER; Schema: public; Owner: boa
 --
 
@@ -201,10 +249,10 @@ CREATE TRIGGER refresh_jobs_update_at_column BEFORE UPDATE ON jobs FOR EACH ROW 
 
 
 --
--- Name: refresh_logs_update_at_column; Type: TRIGGER; Schema: public; Owner: boa
+-- Name: refresh_jobstates_update_at_column; Type: TRIGGER; Schema: public; Owner: boa
 --
 
-CREATE TRIGGER refresh_logs_update_at_column BEFORE UPDATE ON logs FOR EACH ROW EXECUTE PROCEDURE refresh_update_at_column();
+CREATE TRIGGER refresh_jobstates_update_at_column BEFORE UPDATE ON jobstates FOR EACH ROW EXECUTE PROCEDURE refresh_update_at_column();
 
 
 --
@@ -215,19 +263,33 @@ CREATE TRIGGER refresh_platforms_update_at_column BEFORE UPDATE ON platforms FOR
 
 
 --
--- Name: job_environments_environment_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+-- Name: refresh_scenarios_update_at_column; Type: TRIGGER; Schema: public; Owner: boa
 --
 
-ALTER TABLE ONLY job_environments
-    ADD CONSTRAINT job_environments_environment_id_fkey FOREIGN KEY (environment) REFERENCES environments(uuid) ON DELETE CASCADE;
+CREATE TRIGGER refresh_scenarios_update_at_column BEFORE UPDATE ON jobs FOR EACH ROW EXECUTE PROCEDURE refresh_update_at_column();
 
 
 --
--- Name: job_environments_job_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+-- Name: verify_jobstates_status; Type: TRIGGER; Schema: public; Owner: boa
 --
 
-ALTER TABLE ONLY job_environments
-    ADD CONSTRAINT job_environments_job_id_fkey FOREIGN KEY (job) REFERENCES jobs(uuid) ON DELETE CASCADE;
+CREATE TRIGGER verify_jobstates_status BEFORE INSERT OR UPDATE ON jobstates FOR EACH ROW EXECUTE PROCEDURE jobstate_status_in_list();
+
+
+--
+-- Name: files_status_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+--
+
+ALTER TABLE ONLY files
+    ADD CONSTRAINT files_status_fkey FOREIGN KEY (jobstate_id) REFERENCES jobstates(id) ON DELETE CASCADE;
+
+
+--
+-- Name: jobs_environment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+--
+
+ALTER TABLE ONLY jobs
+    ADD CONSTRAINT jobs_environment_fkey FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE;
 
 
 --
@@ -235,15 +297,23 @@ ALTER TABLE ONLY job_environments
 --
 
 ALTER TABLE ONLY jobs
-    ADD CONSTRAINT jobs_platform_id_fkey FOREIGN KEY (platform) REFERENCES platforms(uuid) ON DELETE CASCADE;
+    ADD CONSTRAINT jobs_platform_id_fkey FOREIGN KEY (platform_id) REFERENCES platforms(id) ON DELETE CASCADE;
 
 
 --
--- Name: logs_job_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+-- Name: jobs_scenario_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
 --
 
-ALTER TABLE ONLY logs
-    ADD CONSTRAINT logs_job_id_fkey FOREIGN KEY (job) REFERENCES jobs(uuid) ON DELETE CASCADE;
+ALTER TABLE ONLY jobs
+    ADD CONSTRAINT jobs_scenario_fkey FOREIGN KEY (scenario_id) REFERENCES scenarios(id) ON DELETE CASCADE;
+
+
+--
+-- Name: status_job_fkey; Type: FK CONSTRAINT; Schema: public; Owner: boa
+--
+
+ALTER TABLE ONLY jobstates
+    ADD CONSTRAINT status_job_fkey FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE;
 
 
 --
