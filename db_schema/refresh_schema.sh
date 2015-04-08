15ae7f7aedafdcde7ec67af742140c4805b952b4
Gonéri Le Bouder <goneri@redhat.com>
2015-04-08 18:25:49 +0200

    schema: generate dci_control_server.png too
    Generate a schema of the dci_control_server database once
    dci-control-server.sql is refreshed.
    
    Change-Id: I20ef67908d4477daee4a5b531529804112353d1d

--- db_schema/refresh_schema.sh
+++ db_schema/refresh_schema.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pg_dump --no-owner -U boa -h 127.0.0.1 -s dci_control_server|sed 's,\ \+$,,' > dci-control-server.sql
+postgresql_autodoc -u boa -d dci_control_server -h 127.0.0.1 --password && dot dci_control_server.dot -Tpng > dci_control_server.png
