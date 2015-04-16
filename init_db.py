e743be58a962f971a613f76daf1e304f0bc2d02a
Gonéri Le Bouder <goneri@redhat.com>
2015-04-16 12:14:26 +0200

    pep8 clean up
    Change-Id: I7c80de184706349564fd323b266a72edd9a6dd2e

--- init_db.py
+++ init_db.py
@@ -14,7 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import server.db.api as api
 from server.db.models import Environment
 from server.db.models import File
 from server.db.models import Job
@@ -28,4 +27,3 @@ session.query(File).delete()
 session.query(Environment).delete()
 session.query(Platform).delete()
 session.query(Scenario).delete()
-
