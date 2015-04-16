e31c7ef0670a314e2f089fd5f03c80d9b5f69f98
Gonéri Le Bouder <goneri@redhat.com>
2015-04-16 23:10:54 +0200

    update the sample.sh
    - avoid hard-coded server name, use $DCI_CONTROL_SERVER instead
    - prepare notfications
    
    Change-Id: I459b2a1787878013d299185e8505dbc04e84cb83

--- sample/sample.sh
+++ sample/sample.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/bash
 
+set -eux
 
-export DCI_CONTROL_SERVER=http://dci-boboa.rhcloud.com
-#DCI_CONTROL_SERVER=http://127.0.0.1:5000
+DCI_CONTROL_SERVER=http://dci-boboa.rhcloud.com
+# DCI_CONTROL_SERVER=http://127.0.0.1:5000
+
+export DCI_CONTROL_SERVER
 
 git push openshift master:master -f
 
-
 curl -H "Content-Type: application/json" -X POST -d '[{"name": "SPS-I.1.3-RH7.0-3nodes", "url": "http://boa-2/environments/13758242/"}]' $DCI_CONTROL_SERVER/environments
-
-
-curl -H "Content-Type: application/json" -X POST -d @sample/sample-scenario.json $DCI_CONTROL_SERVER/scenarios
 
 curl -H "Content-Type: application/json" -X POST -d '[{"name": "boa-2"}]' $DCI_CONTROL_SERVER/platforms
 
-curl 'http://dci-boboa.rhcloud.com/platforms?where{"name":"boa-2"}'|jq '._items[0]'
+curl $DCI_CONTROL_SERVER/platforms?where{"name":"boa-2"}|jq '._items[0]'
 
-curl 'http://dci-boboa.rhcloud.com/platforms/boa-2'|jq '.'
+curl $DCI_CONTROL_SERVER/platforms/boa-2|jq '.'
+#
+curl $DCI_CONTROL_SERVER/environments?where{"name":"SPS-I.1.3-RH7.0-3nodes"}|jq '.'
+environment_id=$(curl $DCI_CONTROL_SERVER/environments?where{"name":"SPS-I.1.3-RH7.0-3nodes"}|jq '._items[0].id'|sed 's,",,g')
+echo $environment_id
 
-platform_id=$(curl 'http://dci-boboa.rhcloud.com/platforms?where{"name":"boa-2"}'|jq '._items[0].id'|sed 's,",,')
+curl -H "Content-Type: application/json" -X POST -d '[{"environment_id":"'$environment_id'","struct":{"type":"gerrit","server":"softwarefactory.enovance.com","account":"goneri","gitsha1":"e743be58a962f971a613f76daf1e304f0bc2d02a","port":29418}}]' $DCI_CONTROL_SERVER/notifications
+
+
+platform_id=$(curl "$DCI_CONTROL_SERVER/platforms?where{\"name\":\"boa-2\"}"|jq '._items[0].id'|sed 's,",,g')
 
 client/dci_client.py auto $platform_id
-
-curl http://$DCI_CONTROL_SERVER.com/jobstates|jq '._items'
