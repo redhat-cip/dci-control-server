e186a4d195b75d007304938d85535979b70f53ea
Gonéri Le Bouder <goneri@redhat.com>
2015-05-13 15:44:59 +0200

    server-rdo.sh
    Change-Id: I77a403c593f0bd32b25c2bc15c87ff5f9385add0

--- sample/server-rdo.sh
+++ sample/server-rdo.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -eux
-DCI_CONTROL_SERVER='https://stable-dcics.rhcloud.com'
-CURL="curl -H 'Content-Type: application/json' --silent"
+DCI_CONTROL_SERVER=${DCI_CONTROL_SERVER:-'https://stable-dcics.rhcloud.com'}
+CURL="curl --user 'admin:admin' -H 'Content-Type: application/json' --silent"
 
 report_url="http://trunk-mgt.rdoproject.org/repos/report.html"
 report_regex='s,.*\(.\{46\}_.\{8\}\)_\(.\{10\}\).*,http://trunk-mgt.rdoproject.org/repos/\1 \2,'
 
-curl -X DELETE ${DCI_CONTROL_SERVER}/products
-curl -X DELETE ${DCI_CONTROL_SERVER}/jobs
+curl --user 'admin:admin' -X DELETE ${DCI_CONTROL_SERVER}/products
+curl --user 'admin:admin' -X DELETE ${DCI_CONTROL_SERVER}/jobs
 
 # create a remote ci
-remoteci_id=$(curl -H 'Content-Type: application/json' --silent -X POST -d '[{"name": "rhci"}]' ${DCI_CONTROL_SERVER}/remotecis |jq -r '.id')
+remoteci_id=$(curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d '[{"name": "rhci"}]' ${DCI_CONTROL_SERVER}/remotecis |jq -r '.id')
 
 echo "remoteci_id: $remoteci_id"
 
 # create a product
-product_id=$(curl -H 'Content-Type: application/json' --silent -X POST -d '[{"name": "packstack-rdo", "data": {"ksgen_args": {"rules-file": "%%KHALEESI_SETTINGS%%/rules/packstack-rdo-aio.yml"}}}]' ${DCI_CONTROL_SERVER}/products |jq -r '.id')
+product_id=$(curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d '[{"name": "packstack-rdo", "data": {"ksgen_args": {"rules-file": "%%KHALEESI_SETTINGS%%/rules/packstack-rdo-aio.yml"}}}]' ${DCI_CONTROL_SERVER}/products |jq -r '.id')
 
 echo "product_id: $product_id"
 
 # create a test
-test_id=$(curl -H 'Content-Type: application/json' --silent -X POST -d '[{"data": {"ksgen_args": {"provisioner-options": "execute_provision", "product-version-workaround": "centos-7.0", "provisioner": "openstack", "distro": "centos-7.0", "tester": "tempest", "installer-network-variant": "ml2-vxlan", "product-version": "kilo", "tester-setup": "rpm", "installer-network": "neutron", "tester-tests": "all", "product-version-repo": "delorean", "workarounds": "enabled"}}, "name":"centos-7.0"}]' ${DCI_CONTROL_SERVER}/tests |jq -r '.id')
+test_id=$(curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d '[{"data": {"ksgen_args": {"provisioner-options": "execute_provision", "product-version-workaround": "centos-7.0", "provisioner": "openstack", "distro": "centos-7.0", "tester": "tempest", "installer-network-variant": "ml2-vxlan", "product-version": "kilo", "tester-setup": "rpm", "installer-network": "neutron", "tester-tests": "all", "product-version-repo": "delorean", "workarounds": "enabled"}}, "name":"centos-7.0"}]' ${DCI_CONTROL_SERVER}/tests |jq -r '.id')
 
 echo "test_id: $test_id"
 
@@ -31,15 +31,15 @@ lynx -dump ${report_url}|awk '/rpmbuild.log/ {print $2}'|sed "${report_regex}"| 
 	# create a version
 	json='[{"product_id": "'${product_id}'", "data": {"ksgen_args": {"extra-vars": {"product.repo.delorean_mgt_pin_version": "'${url}'", "product.repo.delorean_pin_version": "'${url}'", "product.repo.delorean.repo_file": "delorean.repo"}}}, "name": "trunk-mgt-'${date}'"}]'
 	echo ${json}
-	# curl -H 'Content-Type: application/json' --silent -X POST -d "${json}" ${DCI_CONTROL_SERVER}/versions
-	ret=$(echo $json|curl -H 'Content-Type: application/json' --silent -X POST -d "${json}" ${DCI_CONTROL_SERVER}/versions)
+	# curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d "${json}" ${DCI_CONTROL_SERVER}/versions
+	ret=$(echo $json|curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d "${json}" ${DCI_CONTROL_SERVER}/versions)
 	echo ${ret}|jq .
 	version_id=$(echo ${ret}|jq -r '.id')
 
 	echo "version_id: $version_id"
 
 	# associate a test to a version
-	test_version_id=$(curl -H 'Content-Type: application/json' --silent -X POST -d '[{"test_id": "'${test_id}'", "version_id": "'${version_id}'"}]' ${DCI_CONTROL_SERVER}/testversions |jq -r '.id')
+	test_version_id=$(curl --user 'admin:admin' -H 'Content-Type: application/json' --silent -X POST -d '[{"test_id": "'${test_id}'", "version_id": "'${version_id}'"}]' ${DCI_CONTROL_SERVER}/testversions |jq -r '.id')
 
 	echo "test_version: ${test_version_id}"
 
