f8c800e314573b58a65c3a2532ac10e260d09a7a
Gonéri Le Bouder <goneri@redhat.com>
2015-05-28 21:37:57 +0200

    WIP: DCIClient: a wrapper class on top of requests
    DCIClient is a wrapper on top of requests that aim to simplify the
    access to the REST API.
    
    Change-Id: I5cb4221b383bb7b48763be816b8d3e32dc471d37

---
+++ client/__init__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import requests
+
+
+class DCIClient(object):
+
+    def __init__(self, end_point, login, password):
+        self.end_point = end_point
+        self.s = requests.Session()
+        self.s.headers.setdefault('Content-Type', 'application/json')
+        self.s.auth = (login, password)
+
+    def delete(self, path):
+        return self.s.delete("%s%s" % (self.end_point, path))
+
+    def post(self, path, data):
+        r = self.s.post("%s%s" % (self.end_point, path), data=json.dumps(data))
+        print(r.json())
+        return r.json()['id']
+
+    def get(self, path):
+        r = self.s.get("%s%s" % (self.end_point, path))
+        return r.json()
+
+    def upload_file(self, fd, jobstate_id, mime='text/plain', name=None):
+        print("upload_file")
+        fd.seek(0)
+        output = ""
+        for l in fd:
+            output += l.decode("UTF-8")
+            data = {"name": name,
+                    "content": output,
+                    "mime": mime,
+                    "jobstate_id": jobstate_id}
+        return self.post("/files", data)
+
