faff016b186d8867368aa7055dfd73b3dc973551
Gonéri Le Bouder <goneri@redhat.com>
2015-06-05 14:59:57 +0200

    test: app.py coverage
    Initial version of the test_app.py file. We still have to cover a lot of
    different cases:
     - get
     - patch
     - delete
     - item post
    
    Change-Id: Ibab231ab4eaab66d7396e5cbf636a74417894469

---
+++ server/tests/test_app.py
@@ -0,0 +1,81 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+import server.tests
+
+
+class TestApp(server.tests.DCITestCase):
+    @staticmethod
+    def _extract_response(rv):
+        return json.loads(rv.get_data().decode())
+
+    def test_post_product_item(self):
+        rv = self.admin_client(
+            'post',
+            '/api/products',
+            data={'name': 'bob',
+                  'data': {
+                      'product_keys': {
+                          'foo': ['bar1', 'bar2']}}})
+        self.assertEqual(rv.status_code, 201)
+        response = self._extract_response(rv)
+        self.assertTrue(response['id'])
+        return response
+
+    def test_post_version_item(self):
+        product = self.test_post_product_item()
+        rv = self.admin_client(
+            'post',
+            '/api/versions',
+            data={'name': 'bob',
+                  'product_id': product['id'],
+                  'data': {
+                      'version_keys': {
+                          'foo': ['bar1', 'bar2']}}})
+        self.assertEqual(rv.status_code, 201)
+        response = self._extract_response(rv)
+        self.assertTrue(response['id'])
+        return response
+
+    def test_post_test_item(self):
+        rv = self.admin_client(
+            'post',
+            '/api/tests',
+            data={
+                'name': 'bob',
+                'data': {
+                    'test_keys': {
+                        'foo': ['bar1', 'bar2']}}})
+        self.assertEqual(rv.status_code, 201)
+        response = self._extract_response(rv)
+        self.assertTrue(response['id'])
+        return response
+
+    def test_post_testversion_item(self):
+        test = self.test_post_test_item()
+        version = self.test_post_version_item()
+        rv = self.admin_client(
+            'post',
+            '/api/testversions',
+            data={
+                'test_id': test_id,
+                'version_id': version['id']})
+        self.assertEqual(rv.status_code, 201)
+        response = self._extract_response(rv)
+        self.assertTrue(response['id'])
+        return response
