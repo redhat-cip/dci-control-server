aa03e51938702ff058c5decc721bc64f944312fa
Gonéri Le Bouder <goneri@redhat.com>
2015-06-05 14:53:34 +0200

    test: recreate a fresh db from setUp()
    Change-Id: I12c09408e25fb0f17b6ac6824f040689b55414bf

--- server/tests/__init__.py
+++ server/tests/__init__.py
@@ -15,20 +15,50 @@
 # under the License.
 
 import base64
+import json
+import os
+import shutil
+import subprocess
+import tempfile
+import time
 
 import server
 import testtools
-
-import server.app as app
 
 
 class DCITestCase(testtools.TestCase):
 
     def setUp(self):
         super(DCITestCase, self).setUp()
+        self._db_dir = tempfile.mkdtemp()
+        subprocess.call(['initdb', '--no-locale', '--nosync', self._db_dir])
+        subprocess.call(['sed', '-i',
+                         "s,#listen_addresses.*,listen_addresses = '',",
+                         '%s/postgresql.conf' % self._db_dir])
+        self._pg = subprocess.Popen(['postgres', '-F',
+                                     '-k', self._db_dir,
+                                     '-D', self._db_dir])
+        time.sleep(1)
+        subprocess.call(['psql', '--quiet',
+                         '--echo-hidden', '-h', self._db_dir,
+                         '-f', 'db_schema/dci-control-server.sql',
+                         'template1'])
+        time.sleep(1)
+        db_uri = (
+            "postgresql:///?host=%s&dbname=template1" % self._db_dir)
+        # TODO(Gonéri): We should set the DB only at one place
+        os.environ['OPENSHIFT_POSTGRESQL_DB_URL'] = db_uri
+        import server.app as app
         self.app = app
         server.app.app.config['TESTING'] = True
+        server.app.app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
         self.test_client = self.app.app.test_client()
+
+    def tearDown(self):
+        super(DCITestCase, self).tearDown()
+        self._pg.kill()
+        time.sleep(2)
+        shutil.rmtree(self._db_dir)
 
     def client_call(self, method, username, password, *argv):
         encoded_basic_auth = base64.b64encode(
