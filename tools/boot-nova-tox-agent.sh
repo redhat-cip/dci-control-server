4c01201e9751bfd7d96695fad7bf69aade2c48ff
Gonéri Le Bouder <goneri@redhat.com>
2015-06-26 14:33:39 +0200

    helper to run tox agent in OpenStack
    boot-nova-tox-agent.sh will create a pool of 8 VM on OpenStack, install tox on
    team and run the tox-agent.
    
    Change-Id: I6f83dad27b424ee649d959f5e7e01ac93f866821

---
+++ tools/boot-nova-tox-agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+while true; do
+    while [ $(nova list|awk '/(BUILD|ACTIVE)/ {print $2}'|wc -l) -lt "8" ]; do
+        nova boot --flavor m1.small --image 'Fedora 22 Cloud x86_64 (RAW)' --key-name goneri --user-data=cloud-init.sh dci-tox-agent-$RANDOM
+        sleep 1
+    done
+    for vm in $(nova list|awk '/ACTIVE/ {print $2}'); do
+        if nova console-log $vm|grep 'System halted'; then
+            nova delete $vm
+        fi
+    done
+    sleep 60
+done
---
+++ tools/cloud-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo 'nameserver 8.8.4.4' > /etc/resolv.conf
+
+yum distro-sync -y
+yum install -y nodejs npm python-tox postgresql-server postgresql-devel postgresql-contrib python-devel python3-devel libffi-devel git python-simplejson
+npm install jscs -g
+
+# Start agent for 1 hour, then stop the node.
+cat > /home/fedora/run.sh << 'EOF'
+#!/bin/bash
+[ -d dci-control-server ] || git clone https://github.com/enovance/dci-control-server
+cd dci-control-server
+start_date=$(date +%s)
+
+while [ $(date +%s) -lt $((start_date+3600)) ]; do
+    PYTHONPATH=. DCI_CONTROL_SERVER=https://stable-dcics.rhcloud.com/api DCI_LOGIN=partner DCI_PASSWORD=partner ./sample/tox-agent.py tox-agent-boa-$(hostname)
+    PYTHONPATH=. DCI_CONTROL_SERVER=https://staging-dcics.rhcloud.com/api DCI_LOGIN=partner DCI_PASSWORD=partner ./sample/tox-agent.py tox-agent-boa-$(hostname)
+    sleep 4
+done
+EOF
+chown fedora:fedora /home/fedora/run.sh
+chmod +x /home/fedora/run.sh
+su - fedora /home/fedora/run.sh &
