7338ac1957b8da22cef67ea4004bfc4018a189e3
Gonéri Le Bouder <goneri@redhat.com>
2015-07-17 16:39:34 +0200

    lookup the versions using the sha
    product/sha has got an unicity key. Ensure we do not recreate another
    record with the same sha.
    
    Change-Id: Id7aab6a29ebdc239d1c6e39b7435f5b838dab352

--- sample/tox-register-sha2.py
+++ sample/tox-register-sha2.py
@@ -33,7 +33,7 @@ def sha_walker(sha_to_walks, dci_client, repository, product_id, test_id):
             sha_to_walks.append(parent['sha'])
 
     # NOTE(Gonéri): Is the commit already here?
-    r = dci_client.get("/versions/%s" % sha)
+    r = dci_client.get("/versions", where={'sha': sha})
     if r.status_code == 404:
         message = commit['message']
         title = message.split('\n')[0]
