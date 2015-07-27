071c18cd335792ffade9c2808c7fa9e3fc56a33e
Gonéri Le Bouder <goneri@redhat.com>
2015-07-27 17:31:35 -0400

    beging gerrithub.io integration
    Redesign the data structure to be able to pin on a given git version or
    a componant.
    
    I think we should later move the JSON part that describe the
    product repository in a sub components section. For example:
    
    - data/repositories/dci-control-server/git
    - data/repositories/dci-control-server/rev
    
    And the test configuration should be in another section. Like:
    
    - data/tests/tox
    - data/tests/khaleesi
    
    This way, it will be easier to see the difference between the
    environment that has to be prepared before the test and the test
    configuration.
    
    Change-Id: I9959dabb63ad6576464992837ab0e97400a5b38d

--- sample/sync-gerrit-review.py
+++ sample/sync-gerrit-review.py
@@ -22,86 +22,154 @@
 # in Gerrit (-1/0/+1)
 
 import subprocess
-import sys
 import yaml
+
 
 import client
 
-dci_project = "dci-control-server"
 
-reviews = subprocess.check_output(['ssh', '-xp29418',
-                                   'softwarefactory.enovance.com',
-                                   'gerrit', 'query', '--format=json',
-                                   'project:%s' % dci_project, 'status:open'])
+def list_open_patchset(dci_client, project):
+    """Generator that return a patchset for a given project."""
+    gerrit_server = project['gerrit_server']
+    product_project = project['product_project']
 
-dci_client = client.DCIClient()
-products = dci_client.get('/products',
-                          where={'name': dci_project}).json()
-try:
-    product = products['_items'][0]
-except KeyError:
-    print("Cannot find the product")
-    sys.exit(1)
+    reviews = subprocess.check_output(['ssh', '-xp29418',
+                                       gerrit_server,
+                                       'gerrit', 'query', '--format=json',
+                                       'project:%s' % product_project,
+                                       'status:open'])
+    for line in reviews.decode('utf-8').rstrip().split('\n'):
+        review = yaml.load(line)
+        if 'id' not in review:
+            continue
+        patchset_query_res = subprocess.check_output([
+            'ssh', '-xp29418', gerrit_server,
+            'gerrit', 'query', '--format=JSON',
+            '--current-patch-set change:%d' % int(review['number'])])
+        patchset = yaml.load(patchset_query_res.decode('utf-8').split('\n')[0])
+        yield patchset
 
-test = dci_client.get('/tests/tox').json()
 
-for line in reviews.decode('utf-8').rstrip().split('\n'):
-    review = yaml.load(line)
-    if 'id' not in review:
-        continue
-    patchset_query_res = subprocess.check_output([
-        'ssh', '-xp29418', 'softwarefactory.enovance.com',
-        'gerrit', 'query', '--format=JSON',
-        '--current-patch-set change:%d' % int(review['number'])])
-    patchset = yaml.load(patchset_query_res.decode('utf-8').split('\n')[0])
+def push_patchset_in_dci(dci_client, product, test, patchset, git_url):
+    """Create a version in DCI-CS from a gerrit patchset."""
     subject = patchset['commitMessage'].split('\n')[0]
     message = patchset['commitMessage']
     gerrit_id = patchset['id']
     url = patchset['url']
     ref = patchset['currentPatchSet']['ref']
     sha = patchset['currentPatchSet']['revision']
-    versions = dci_client.get("/versions", where={'sha': sha}).json()
-
-    if len(versions['_items']) == 0:
-        r = dci_client.post("/versions", {
-            "product_id": product['id'],
-            "name": subject,
-            "title": subject,
-            "message": message,
-            "sha": sha,
-            "url": url,
-            "data": {
-                "git_url": (
-                    "http://softwarefactory.enovance.com/r/%s" % dci_project),
+    print("Gerrit → DCI-CS: %s" % subject)
+    version_data = {
+        "product_id": product['id'],
+        "name": subject,
+        "title": subject,
+        "message": message,
+        "sha": sha,
+        "url": url,
+        "data": {
+            product_name: {
+                "git": git_url,
                 "ref": ref,
                 "sha": sha,
                 "gerrit_id": gerrit_id
             }
-        })
-        version = r.json()
-        dci_client.post("/testversions", {
-            "test_id": test['id'],
-            "version_id": version['id'],
-        })
-    else:
-        version = versions['_items'][0]
-        testversions = dci_client.get(
-            "/testversions",
-            where={'version_id': version['id']}).json()
-        status = '0'
-        for testversion in testversions['_items']:
-            builds = dci_client.get(
-                "/jobs",
-                where={'testversion_id': testversion['id']},
-                embedded={'jobstates_collection': 1}).json()
-            for build in builds['_items']:
-                if build['jobstates_collection'][-1]['status'] == 'failure':
-                    status = '-1'
-                    break
-                elif build['jobstates_collection'][-1]['status'] == 'success':
-                    status = '1'
-        subprocess.check_output([
-            'ssh', '-xp29418', 'softwarefactory.enovance.com',
-            'gerrit', 'review', '--verified', status,
-            version['data']['sha']])
-        print("%s: %s" % (subject, status))
+        }
+    }
+    version = dci_client.find_or_create_or_refresh(
+        '/versions', version_data, unicity_key=['sha'])
+    dci_client.find_or_create_or_refresh(
+        '/testversions',
+        {'test_id': test['id'], 'version_id': version['id']},
+        unicity_key=['test_id', 'version_id'])
+    return version
+
+
+def review_patchset(dci_client, version):
+    """Update the review in Gerrit from the status of a version in DCI-CS."""
+    testversions = dci_client.get(
+        "/testversions",
+        where={'version_id': version['id']}).json()
+    status = '0'
+    for testversion in testversions['_items']:
+        jobs = dci_client.get(
+            "/jobs",
+            where={'testversion_id': testversion['id']},
+            embedded={'jobstates_collection': 1}).json()
+        for job in jobs['_items']:
+            last_job_status = job['jobstates_collection'][-1]['status']
+            if last_job_status == 'failure':
+                status = '-1'
+                break
+            elif last_job_status == 'success':
+                status = '1'
+    try:
+        sha = version['data'][product_name]['sha']
+    except KeyError:
+        print("Cannot find product name for version "
+              "%s" % version['id'])
+        return
+    subprocess.check_output([
+        # NOTE(Gonéri): The echo is here on purpose
+        'ssh', '-xp29418', gerrit_server,
+        'gerrit', 'review', '--verified', status,
+        sha])
+
+# NOTE(Gonéri): this configuration structure should probably be exported in
+# some flat configuration files.
+projects = [
+    {
+        'gerrit_server': 'review.gerrithub.io',
+        'product_name': 'khaleesi',
+        'product_project': 'redhat-openstack/khaleesi',
+        'test_name': 'khaleesi-tempest',
+        'git_url_format': 'http://{server}/{project}',
+        'product_data': {
+            "ksgen_args": {
+                "provisioner": "manual",
+                "product": "rdo",
+                "product-version": "kilo",
+                "product-version-repo": "delorean",
+                "product-version-workaround": "rhel-7.0",
+                "workarounds": "enabled",
+                "distro": "centos-7.0",
+                "installer": "rdo_manager",
+                "installer-env": "virthost",
+                "installer-topology": "minimal",
+                "extra-vars": ["product.repo_type_override=none"]},
+            'khaleesi': {
+                'git': 'http://github.com/redhat-openstack/khaleesi'},
+            'khaleesi-settings': {
+                'git': 'http://github.com/redhat-openstack/khalees-settings'}
+        }},
+    {
+        'gerrit_server': 'softwarefactory.enovance.com',
+        'product_name': 'dci-control-server',
+        'product_project': 'dci-control-server',
+        'test_name': 'tox',
+        'publish_review': True
+    }
+]
+
+dci_client = client.DCIClient()
+for project in projects:
+    product_name = project['product_name']
+    product_data = project.get('product_data', {})
+    test_name = project['test_name']
+    git_url_format = project.get('git_url_format',
+                                 'http://{server}/r/{project}')
+    gerrit_server = project['gerrit_server']
+    product_project = project['product_project']
+
+    product = dci_client.find_or_create_or_refresh('/products', {
+        'name': product_name, 'data': product_data})
+    test = dci_client.find_or_create_or_refresh('/tests', {
+        'name': test_name, 'data': {}})
+
+    git_url = git_url_format.format(server=gerrit_server,
+                                    project=product_project)
+    for patchset in list_open_patchset(dci_client, project):
+        version = push_patchset_in_dci(
+            dci_client, product,
+            test, patchset, git_url)
+        if project.get('publish_review', False):
+            review_patchset(dci_client, version)
