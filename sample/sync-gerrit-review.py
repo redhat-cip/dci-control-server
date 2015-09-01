6196224e6d1a88b152ced3783f5786c4056b8052
Yassine Lamgarchal <yassine.lamgarchal@enovance.com>
2015-09-01 17:08:08 +0200

    Review only if the status is different from 0.
    Change-Id: I8effde484fad00f04c010c6b6b798ec84ebcf746

--- sample/sync-gerrit-review.py
+++ sample/sync-gerrit-review.py
@@ -135,8 +135,9 @@ def review_patchset(dci_client, project, version):
               "%s" % version['id'])
         return
     # TODO(Gonéri): also push a message and the URL to see the job.
-    print("DCI-CS → Gerrit: %s" % status)
-    _gerrit_review(gerrit_server, sha, status)
+    if status != '0':
+        print("DCI-CS → Gerrit: %s" % status)
+        _gerrit_review(gerrit_server, sha, status)
 
 # NOTE(Gonéri): This structure should be in a configuration files instead.
 products = {
