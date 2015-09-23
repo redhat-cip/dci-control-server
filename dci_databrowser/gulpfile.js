9b3b5cb991f466d3804fb9815be93afa38c34f62
Maxime Vidori <mvidori@redhat.com>
2015-09-23 15:22:51 +0200

    Add a livereload server
    Change-Id: Iec3833d1f365be666a77d8aeaecd3daa5457b62f

--- dci_databrowser/gulpfile.js
+++ dci_databrowser/gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 var jscs = require('gulp-jscs');
+var config = require('./config');
 
 gulp.task('jscs', function() {
     return gulp.src(['src/**.js', 'gulpfile.js'])
@@ -11,5 +13,33 @@ gulp.task('jscs', function() {
 gulp.task('build', function() {
     return gulp.src('src/**').pipe(gulp.dest('static'));
 });
+
+gulp.task('reload', ['build'], function() {
+    return gulp.src('static/**').pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**', ['reload']);
+});
+
+gulp.task('connect', function() {
+    var url = require('url');
+    var proxy = require('proxy-middleware');
+
+    return connect.server({
+        host: config.host,
+        port: config.port,
+        livereload: true,
+        root: 'static',
+        middleware: function(connect, opt) {
+            var options = url.parse(config.api);
+            options.route = '/api';
+
+            return [proxy(options)];
+        }
+    });
+});
+
+gulp.task('serve', ['build', 'watch', 'connect']);
 
 gulp.task('test', ['jscs']);
