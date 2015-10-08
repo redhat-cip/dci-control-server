bd7368c00e972565931eeb591f8b96396df3da00
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-10-08 18:18:42 +0200

    Update sample/sample.py
    Change-Id: Id8f44d68563feb45815c9a7b70235ffc2ea1d56c

--- sample/sample.py
+++ sample/sample.py
@@ -24,21 +24,18 @@ import uuid
 client = client.DCIClient(end_point="http://127.0.0.1:5000",
                           login="admin", password="admin")
 
-for _ in range(10):
-    product = client.post('/api/products',
-                          data={'name': str(uuid.uuid4())[:18],
-                                'data': {
-                                'product_keys': {
-                                    'foo': ['bar1', 'bar2']}}})
-    product_id = product.json()["id"]
+componenttype_id = client.post('/api/componenttypes',
+                               data={'name': 'kikoolol'}).json()["id"]
 
-    version = client.post('/api/versions',
-                          data={'name': str(uuid.uuid4())[:18],
-                                'product_id': product_id,
-                                'data': {
-                                'version_keys': {
-                                    'foo': ['bar1', 'bar2']}}})
-    version_id = version.json()["id"]
+team_id = client.post('/api/teams', data={'name': 'team'}).json()["id"]
+
+for _ in range(10):
+    component = client.post('/api/components',
+                            data={'name': str(uuid.uuid4())[:18],
+                                  'data': {'lol': 'looool'},
+                                  'componenttype_id': componenttype_id,
+                                  'canonical_project_name': 'mdr'})
+    component_id = component.json()["id"]
 
     for _ in range(5):
 
@@ -49,16 +46,17 @@ for _ in range(10):
                                          'foo': ['bar1', 'bar2']}}})
         test_id = test.json()["id"]
 
-        testversion = client.post('/api/testversions',
-                                  data={
-                                      'test_id': test_id,
-                                      'version_id': version_id})
-        testversion_id = testversion.json()["id"]
+        jobdefinition = client.post('/api/jobdefinitions',
+                                    data={'name': str(uuid.uuid4())[:18],
+                                          'test_id': test_id})
+        jobdefinition_id = jobdefinition.json()["id"]
+
 
         remoteci = client.post('/api/remotecis',
                                data={
                                    'name': str(uuid.uuid4())[:18],
                                    'test_id': test_id,
+                                   'team_id': team_id,
                                    'data': {
                                        'remoteci_keys': {
                                            'foo': ['bar1', 'bar2']}}})
@@ -66,13 +64,15 @@ for _ in range(10):
 
         job = client.post('/api/jobs',
                           data={'remoteci_id': remoteci_id,
-                                'testversion_id': testversion_id})
+                                'team_id': team_id,
+                                'jobdefinition_id': jobdefinition_id})
         job_id = job.json()["id"]
 
         alea = random.randint(0, 2)
         status = ["ongoing", "failure", "success"][alea]
         jobstate = client.post('/api/jobstates',
                                data={'job_id': job_id,
+                                     'team_id': team_id,
                                      'status': status})
         jobstate_id = jobstate.json()["id"]
 
@@ -81,6 +81,7 @@ for _ in range(10):
                         data={'jobstate_id': jobstate_id,
                               'content': 'kikoolol! mdr! lol!' * 100,
                               'name': str(uuid.uuid4())[:18],
-                              'mime': 'text'})
+                              'mime': 'text',
+                              'team_id': team_id})
 
 print("Database populated successfully :)\n")
