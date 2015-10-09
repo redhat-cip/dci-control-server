6a105b475eef5cf4cbdbf9f05ebfdbbd3a163c42
Gonéri Le Bouder <goneri@redhat.com>
2015-10-09 15:01:37 -0400

    ensure unfinished get properly flag
    If a remoteci starts a new job, we flag the previous unfinished jobs
    as 'unfinished'.
    This commit comes with 4b96667d8e70d636d46b558e01cd81bbc87e39c6.
    
    Change-Id: I8f87c794dc088d725a35133e5f2dd87374b6f9dd

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -14,6 +14,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import pytest
+
 import server.tests.utils as utils
 
 
@@ -113,3 +115,29 @@ class TestAdmin(object):
                 job_to_recheck.data['jobdefinition_id'])
         assert recheck_job.data['team_id'] == job_to_recheck.data['team_id']
         assert recheck_job.data['recheck'] is True
+
+    @pytest.mark.parametrize(('status'), ['new', 'initializing', 'ongoing'])
+    def test_get_job_should_kill_existing_jobs(
+            self, admin, company_a_user, status):
+        """GET /jobs should retrieve a new job and kill the existing ones.
+        """
+        test = utils.create_test(admin)
+        remoteci = utils.create_remoteci(admin, test.data['id'])
+
+        def get_job_status(job_id):
+            r = company_a_user.get(
+                '/api/jobs/%s?embedded={"jobstates": 1}' % job_id)
+            return r.data['jobstates'][0]['status']
+
+        utils.create_jobdefinition(admin, test.data['id'], name=('test_1'))
+        utils.create_jobdefinition(admin, test.data['id'], name=('test_2'))
+
+        first_job = utils.create_job(company_a_user, remoteci.data['id'])
+        utils.create_jobstate(
+            company_a_user, first_job.data['id'], status=status)
+        assert get_job_status(first_job.data['id']) == status
+
+        # create another job for the remoteci, the previous job won't finish
+        # and so, should get the 'unfinished' status.
+        utils.create_job(company_a_user, remoteci.data['id'])
+        assert get_job_status(first_job.data['id']) == 'unfinished'
