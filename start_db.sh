da1bad9bab51ddff164dc31d5cba3981f51d1c1a
Maxime Vidori <mvidori@redhat.com>
2015-10-09 12:08:10 +0200

    Refactoring of all the test suite
    Application now relies on a settings file, this
    settings can be customized or a different one can be loaded
    with the DCI_SETTINGS_MODULE environment variable
    
    No more subprocess for instanciating a test database,
    we now use sqlalchemy_utils.functions for handling this part.
    
    All tests have been refactored according to the pytest
    architecture instead of testtools, the new database is
    session scoped (see pytest scopes).
    
    In order to avoid multiprocessing for client testing
    an Adapter has been made between flask test client and request
    This allows us to directly call server endpoints without starting
    it.
    
    Change-Id: Iea233d74858537b2534c5662e6c2f50475bd81e8

---
+++ start_db.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# checks if pg_ctl command exists
+type "pg_ctl" &> /dev/null || exit 0
+
+# checks if not already running
+pg_ctl status &> /dev/null && exit 0
+
+# else run postgres
+
+OPTIONS="--client-encoding=utf8 --full-page_writes=off \
+    --logging-collector=off --log-destination='stderr'"
+
+pg_ctl -w -o "-F -h '' $OPTIONS" start
