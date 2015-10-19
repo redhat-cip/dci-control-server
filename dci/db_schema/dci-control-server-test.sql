47d204dd44c86cd375db1a43b142a32a0021c8af
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-10-19 20:41:35 +0200

    Do not: Test Eve without gen_uuid.
    Change-Id: I5a98209b86243d2b52c61ba8fdb3a506acadc3ff

--- dci/db_schema/dci-control-server-test.sql
+++ dci/db_schema/dci-control-server-test.sql
@@ -1,33 +1,33 @@
-INSERT INTO teams (name) VALUES ('admin');
-INSERT INTO teams (name) VALUES ('company_a');
-INSERT INTO teams (name) VALUES ('company_b');
+INSERT INTO teams (id, name) VALUES ('3819e401-3472-4ccb-8c3e-35cbf1185dbf', 'admin');
+INSERT INTO teams (id, name) VALUES ('4819e401-3472-4ccb-8c3e-35cbf1185dbf', 'company_a');
+INSERT INTO teams (id, name) VALUES ('5819e401-3472-4ccb-8c3e-35cbf1185dbf', 'company_b');
 
-INSERT INTO users (name, password, team_id)
-VALUES ('admin', crypt('admin', gen_salt('bf', 8)),
+INSERT INTO users (id, name, password, team_id)
+VALUES ('6819e401-3472-4ccb-8c3e-35cbf1185dbf','admin', crypt('admin', gen_salt('bf', 8)),
         (SELECT id FROM teams WHERE name='company_a'));
 
-INSERT INTO users (name, password, team_id)
-VALUES ('company_a_user', crypt('company_a_user', gen_salt('bf', 8)),
+INSERT INTO users (id, name, password, team_id)
+VALUES ('7819e401-3472-4ccb-8c3e-35cbf1185dbf', 'company_a_user', crypt('company_a_user', gen_salt('bf', 8)),
         (SELECT id FROM teams WHERE name='company_a'));
 
-INSERT INTO users (name, password, team_id)
-VALUES ('company_b_user', crypt('company_b_user', gen_salt('bf', 8)),
+INSERT INTO users (id, name, password, team_id)
+VALUES ('8819e401-3472-4ccb-8c3e-35cbf1185dbf', 'company_b_user', crypt('company_b_user', gen_salt('bf', 8)),
         (SELECT id FROM teams WHERE NAME='company_b'));
 
-INSERT INTO roles (name) VALUES ('admin');
-INSERT INTO roles (name) VALUES ('partner');
+INSERT INTO roles (id, name) VALUES ('9819e401-3472-4ccb-8c3e-35cbf1185dbf', 'admin');
+INSERT INTO roles (id, name) VALUES ('5919e401-3472-4ccb-8c3e-35cbf1185dbf', 'partner');
 
-INSERT INTO user_roles (user_id, role_id)
-VALUES ((SELECT id FROM users WHERE name='admin'),
+INSERT INTO user_roles (id, user_id, role_id)
+VALUES ('5a19e401-3472-4ccb-8c3e-35cbf1185dbf', (SELECT id FROM users WHERE name='admin'),
         (SELECT id FROM roles WHERE name='admin'));
 
-INSERT INTO user_roles (user_id, role_id)
-VALUES ((SELECT id FROM users WHERE name='admin'),
+INSERT INTO user_roles (id, user_id, role_id)
+VALUES ('5b19e401-3472-4ccb-8c3e-35cbf1185dbf', (SELECT id FROM users WHERE name='admin'),
         (SELECT id from roles WHERE name='partner'));
 
-insert into user_roles (user_id, role_id)
-values ((select id from users where name='company_a_user'),
+insert into user_roles (id, user_id, role_id)
+values ('5c19e401-3472-4ccb-8c3e-35cbf1185dbf', (select id from users where name='company_a_user'),
         (select id from roles where name='partner'));
-insert into user_roles (user_id, role_id)
-values ((select id from users where name='company_b_user'),
+insert into user_roles (id, user_id, role_id)
+values ('5d19e401-3472-4ccb-8c3e-35cbf1185dbf', (select id from users where name='company_b_user'),
         (select id from roles where name='partner'));
