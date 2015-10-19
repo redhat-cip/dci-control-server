aa5b6e7d852c71708a3bf5efdb192f2af7d8c9e6
Gonéri Le Bouder <goneri@redhat.com>
2015-10-19 11:33:31 -0400

    find_or_create_or_refresh should update with PATCH
    PUT totally destroy the existing resource and break relationship with
    the other resources. This is not what we want. Let's use PATCH instead
    here.
    
    This patch ensure we make use of datetime _without_ timezone, this
    because datetime datetime from Py27 is not able to compare them properly
    and this create error in the server:
    
        can't compare offset-naive and offset-aware datetimes
    
    offset-naive is the newly generated `updated_at` value whereas
    offset-aware is the value from the DB.
    
    Change-Id: I62bc32b18170816ac36cf7c22c62c0f6feb0e365

--- dci/db_schema/dci-control-server.sql
+++ dci/db_schema/dci-control-server.sql
@@ -50,8 +50,8 @@ COMMENT ON FUNCTION refresh_update_at_column() IS 'Refresh the etag and the upda
 
 CREATE TABLE components (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     componenttype_id uuid NOT NULL,
     name character varying(255) NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
@@ -72,8 +72,8 @@ COMMENT ON TABLE components IS 'The components.';
 
 CREATE TABLE componenttypes (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     name character varying(255) NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL
 );
@@ -81,8 +81,8 @@ COMMENT ON TABLE componenttypes IS 'The different type of components.';
 
 CREATE TABLE files (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     name character varying(512) NOT NULL,
     content text NOT NULL,
     mime character varying(100) DEFAULT 'text/plain'::character varying NOT NULL,
@@ -95,8 +95,8 @@ COMMENT ON TABLE files IS 'The output of a command execution. The file is associ
 
 CREATE TABLE jobs (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     jobdefinition_id uuid NOT NULL,
     remoteci_id uuid NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
@@ -107,8 +107,8 @@ COMMENT ON TABLE jobs IS 'An association between a jobdefinition and a remoteci.
 
 CREATE TABLE jobdefinitions (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     name character varying(200),
     priority integer NOT NULL DEFAULT 0,
@@ -118,8 +118,8 @@ COMMENT ON TABLE jobs IS 'A collection of components and a test ready to associa
 
 CREATE TABLE jobdefinition_components (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     component_id uuid NOT NULL,
     jobdefinition_id uuid NOT NULL
@@ -127,8 +127,8 @@ CREATE TABLE jobdefinition_components (
 
 CREATE TABLE jobstates (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     status character varying,
     comment text,
     job_id uuid NOT NULL,
@@ -140,8 +140,8 @@ COMMENT ON COLUMN jobstates.status IS 'ongoing: the job is still running, failur
 
 CREATE TABLE remotecis (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     name character varying(255),
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     team_id uuid NOT NULL,
@@ -152,8 +152,8 @@ COMMENT ON TABLE remotecis IS 'The remote CI agent that process the test.';
 
 CREATE TABLE roles (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     name character varying(100)
 );
@@ -161,8 +161,8 @@ COMMENT ON TABLE roles IS 'The user roles.';
 
 CREATE TABLE teams (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     name character varying(100)
 );
@@ -170,8 +170,8 @@ COMMENT ON TABLE teams IS 'The user team. An user can only be in one team. All t
 
 CREATE TABLE tests (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     name character varying(255) NOT NULL,
     data json
@@ -180,8 +180,8 @@ COMMENT ON TABLE tests IS 'A QA test.';
 
 CREATE TABLE user_remotecis (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     user_id uuid NOT NULL,
     remoteci_id uuid NOT NULL
@@ -190,8 +190,8 @@ COMMENT ON TABLE user_remotecis IS 'experimental';
 
 CREATE TABLE user_roles (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     user_id uuid NOT NULL,
     role_id uuid NOT NULL
@@ -200,8 +200,8 @@ COMMENT ON TABLE user_roles IS 'Relation table between the users and the roles.'
 
 CREATE TABLE users (
     id uuid DEFAULT gen_uuid() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL,
     etag character varying(40) DEFAULT gen_etag() NOT NULL,
     name character varying(100),
     password text,
