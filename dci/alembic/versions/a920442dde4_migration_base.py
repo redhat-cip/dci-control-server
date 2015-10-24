b55e8270ff1b8ead5dc804bc56e7394f37e17c58
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-10-24 15:12:08 +0200

    Add Alembic for db migration scripts.
    Change-Id: I181c1c4ffc206d088ec0299544f5318728fd3179

---
+++ dci/alembic/versions/a920442dde4_migration_base.py
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2015 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Migration base
+
+Revision ID: a920442dde4
+Revises:
+Create Date: 2015-10-24 16:54:05.198250
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'a920442dde4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- dci/server/app.py
+++ dci/server/app.py
@@ -28,6 +28,7 @@ from eve import Eve
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
 import flask
+import sqlalchemy
 from sqlalchemy.sql import text
 
 from dci.dci_databrowser import dci_databrowser
@@ -213,12 +214,17 @@ def generate_conf():
         os.environ.get('DCI_SETTINGS_MODULE') or 'dci.server.settings')
     return conf
 
-
 def handle_api_exception(api_exception):
     response = flask.jsonify(api_exception.to_dict())
     response.status_code = api_exception.status_code
     return response
 
+def get_engine(conf, echo=False):
+    sa_engine = sqlalchemy.create_engine(
+        conf['SQLALCHEMY_DATABASE_URI'],
+        pool_size=20, max_overflow=0, encoding='utf8', convert_unicode=True,
+        echo=echo)
+    return sa_engine
 
 def create_app(conf):
     dci_model = models.DCIModel(conf['SQLALCHEMY_DATABASE_URI'])
