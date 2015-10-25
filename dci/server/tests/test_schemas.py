fd7c9b89bb371306e276f9a851e09adcd048b904
Maxime Vidori <mvidori@redhat.com>
2015-10-25 16:25:03 +0100

    Add Schema validation for componenttype, team and role
    Change-Id: Icaaae9128e9b4e355e5a7f5d95e75bc25f30b456

--- dci/server/tests/test_schemas.py
+++ dci/server/tests/test_schemas.py
@@ -14,8 +14,15 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import copy
+import datetime
+import dci.server.common.exceptions as exceptions
 import dci.server.common.schemas as schemas
 import flask
+import hashlib
+import pytest
+import six
+import uuid
 import voluptuous
 
 
@@ -33,3 +40,157 @@ def test_validation_error_handling(app):
             'errors': {'id': ['required key not provided']}
         }
     }
+
+
+def generate_data(extra=None):
+    data = {
+        'id': uuid.uuid4(),
+        'etag': hashlib.md5().hexdigest(),
+        'name': 'foo',
+        'created_at': datetime.datetime.now(),
+        'updated_at': datetime.datetime.now()
+    }
+
+    data.update(extra or {})
+    data_dump = copy.deepcopy(data)
+
+    data_dump['id'] = str(data_dump['id'])
+    data_dump['created_at'] = data_dump['created_at'].isoformat()
+    data_dump['updated_at'] = data_dump['updated_at'].isoformat()
+    return data, data_dump
+
+
+class SchemaTesting(object):
+    schema = None
+    post_fields = []
+    put_fields = []
+
+    def test_dump(self):
+        data, data_dump = generate_data({'extra': 'bar'})
+        data_dump.pop('extra')
+        assert self.schema(data) == data_dump
+
+    def test_post_extra_data(self):
+        _, data = generate_data()
+
+        data_post = {'extra': 'bar'}
+        # remove extra keys for comprehension
+        for key, value in six.iteritems(data):
+            if key in self.post_fields:
+                data_post[key] = value
+
+        with pytest.raises(exceptions.APIException) as exc:
+            self.schema.post(data_post)
+
+        assert exc.value.payload == {
+            'errors': {'extra': ['extra keys not allowed']}
+        }
+
+    def test_post_missing_data(self):
+        errors = {}
+        # retrieve all post fields
+        for field in self.post_fields:
+            errors[str(field)] = ['required key not provided']
+
+        with pytest.raises(exceptions.APIException) as exc:
+            self.schema.post({})
+
+        assert exc.value.payload == {'errors': errors}
+
+    def test_post_invalid_data(self):
+        """Too complicated to handle this generically,
+        let the user implement the test
+        """
+        raise NotImplementedError
+
+    def test_post(self):
+        expected_data_post, data_post = {}, {}
+        expected_data, data = generate_data()
+
+        for key, value in six.iteritems(data):
+            if key in self.post_fields:
+                data_post[key] = value
+
+        for key, value in six.iteritems(expected_data):
+            if key in self.post_fields:
+                expected_data_post[key] = value
+
+        assert self.schema.post(data_post) == expected_data_post
+
+    def test_put_extra_data(self):
+        _, data = generate_data()
+
+        data_put = {'extra': 'bar'}
+        # remove extra keys for comprehension
+        for key, value in six.iteritems(data):
+            if key in self.put_fields:
+                data_put[key] = value
+
+        with pytest.raises(exceptions.APIException) as exc:
+            self.schema.put(data_put)
+
+        assert exc.value.payload == {
+            'errors': {'extra': ['extra keys not allowed']}
+        }
+
+    def test_put_missing_data(self):
+        errors = {}
+        # retrieve all put fields
+        for field in self.put_fields:
+            errors[str(field)] = ['required key not provided']
+
+        with pytest.raises(exceptions.APIException) as exc:
+            self.schema.put({})
+
+        assert exc.value.payload == {'errors': errors}
+
+    def test_put_invalid_data(self):
+        """Too complicated to handle this generically,
+        let the user implement the test
+        """
+        raise NotImplementedError
+
+    def test_put(self):
+        expected_data_put, data_put = {}, {}
+        expected_data, data = generate_data()
+
+        for key, value in six.iteritems(data):
+            if key in self.put_fields or key in self.post_fields:
+                data_put[key] = value
+
+        for key, value in six.iteritems(expected_data):
+            if key in self.put_fields or key in self.post_fields:
+                expected_data_put[key] = value
+
+        assert self.schema.put(data_put) == expected_data_put
+
+
+class BaseSchemaTesting(SchemaTesting):
+    post_fields = ['name']
+    put_fields = ['id', 'etag']
+
+    def test_post_invalid_data(self):
+        pass
+
+    def test_put_invalid_data(self):
+        with pytest.raises(exceptions.APIException) as exc:
+            self.schema.put({
+                'id': 'invalid_uuid',
+                'etag': 'invalid_etag'
+            })
+        assert exc.value.payload == {'errors': {
+            'id': ['not a valid uuid'],
+            'etag': ['not a valid etag']
+        }}
+
+
+class TestComponentType(BaseSchemaTesting):
+    schema = schemas.component_type
+
+
+class TestTeam(BaseSchemaTesting):
+    schema = schemas.team
+
+
+class TestRole(BaseSchemaTesting):
+    schema = schemas.role
