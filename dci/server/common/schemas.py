2836e03253546b0e22f21128f55524dcd788e315
Maxime Vidori <mvidori@redhat.com>
2015-10-28 19:49:58 +0100

    Add Schema validation for componenttype, team and role
    Change-Id: If9f76795814898a1bf67722f512ae93038dc722c

--- dci/server/common/schemas.py
+++ dci/server/common/schemas.py
@@ -13,25 +13,71 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from __future__ import unicode_literals
 
+import collections
 import dci.server.common.exceptions as exceptions
 import dci.server.utils as utils
-import voluptuous
+import six
+import voluptuous as v
 
 
-class Schema(voluptuous.Schema):
+INVALID_STRING = 'not a valid string'
+INVALID_REQUIRED = 'required key not provided'
+
+
+class Schema(v.Schema):
     """Override voluptuous schema to return our own error"""
+
+    error_messages = {
+        'expected unicode': INVALID_STRING,
+        'expected str': INVALID_STRING,
+    }
+
     def __call__(self, data):
         def format_error(error):
             path = error.path.pop()
+            msg = error.error_message
+            error.error_message = self.error_messages.get(msg, msg)
+
             err = format_error(error) if error.path else [error.error_message]
-            return {str(path): err}
+            return {six.text_type(path): err}
 
         try:
-            super(Schema, self).__call__(data)
-        except voluptuous.MultipleInvalid as exc:
+            return super(Schema, self).__call__(data)
+        except v.MultipleInvalid as exc:
             errors = {}
             for error in exc.errors:
                 errors = utils.dict_merge(errors, format_error(error))
             raise exceptions.DCIException('Request malformed',
                                           {'errors': errors})
+
+
+DCISchema = collections.namedtuple('DCISchema', ['post', 'put'])
+
+
+def schema_factory(schema):
+    schema_post = {}
+
+    for key, value in six.iteritems(schema):
+        if not isinstance(key, v.Marker):
+            key = v.Required(key)
+
+        schema_post[key] = value
+
+    return DCISchema(Schema(schema_post), Schema(schema))
+
+###############################################################################
+#                                                                             #
+#                                 Base schemas                                #
+#                                                                             #
+###############################################################################
+
+base = {
+    'name': six.text_type,
+}
+
+
+component_type = schema_factory(base)
+team = schema_factory(base)
+role = schema_factory(base)
