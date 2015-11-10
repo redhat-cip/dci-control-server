de91760f941083f7a9eeb40e6eea0ca267441db6
Maxime Vidori <mvidori@redhat.com>
2015-11-10 15:43:08 +0100

    Add licence and stric mode on all js files
    Add a licence banner on all js files
    Also add a "use strict" and correct possible errors
    
    Change-Id: I1f0675fb807618559e953a54fde6e555e5dd4e6e

--- dci/dci_databrowser/src/js/routing.js
+++ dci/dci_databrowser/src/js/routing.js
@@ -1,3 +1,18 @@
+// Copyright 2015 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License'); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+'use strict';
 
 require('./app.js')
 .config([
@@ -129,7 +144,8 @@ require('./app.js')
   '$rootScope', '$state', 'authStates',
   function($rootScope, $state, authStates) {
     $rootScope.$on(
-      '$stateChangeError', function(_, toState, toParams, _, _, error) {
+      '$stateChangeError',
+      function(event, toState, toParams, fromState, fromParams, error) {
         if (error === authStates.DISCONNECTED) {
           $state.go('login', {
             next: toState.name,
