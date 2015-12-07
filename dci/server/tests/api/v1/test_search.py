06c6fbd0ce963d15c2a3ffc55e78c7a1a7f548d1
Cedric LECOMTE <clecomte@redhat.com>
2015-12-07 11:33:16 -0500

    [ES] Adding ACL on search
    If a user is not SUPER_ADMIN he can just search inside is team logs
    
    Change-Id: I3949256a43bfa979692ad0ac8d2133241f1c6944

---
+++ dci/server/tests/api/v1/test_search.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def test_index_file(admin, jobstate_id, team_id, file_id):
+    file = admin.post('/api/v1/search',
+                      data={'pattern': 'kikoolol'}).data
+    result = [item for item in file['logs']['hits'] 
+              if item["_id"] == file_id]
+    assert file_id == result[0]["_id"]
+
+
+def test_get_indexed_file_isolation(user, admin, jobstate_id, team_id,
+                                    remoteci_id, file_id):
+    search = user.post('/api/v1/search',
+                       data={'pattern': 'kikoolol'}).data
+    result = [item for item in search['logs']['hits']
+              if item["_id"] == file_id]
+    assert result == []
