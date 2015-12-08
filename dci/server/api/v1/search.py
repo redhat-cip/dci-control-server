cf8bf607a2774315ce4460c187166749f7955cae
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-12-08 14:24:45 +0100

    Improve access rights computation.
    This commit make more clear the access rights
    of a user according to its role and team.
    
    Change-Id: Ie22031584009c2ab749f3e6dbfc1f7fd1d7e12f7

--- dci/server/api/v1/search.py
+++ dci/server/api/v1/search.py
@@ -23,7 +23,7 @@ from dci.server.common import schemas
 
 
 @api.route('/search', methods=['POST'])
-@auth2.requires_auth()
+@auth2.requires_auth
 def search():
     values = schemas.search.post(flask.request.json)
 
