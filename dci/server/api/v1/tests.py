d09af7e73205be0be4c1dd43e941eb424c302dcf
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-12-08 14:24:45 +0100

    Improve access rights computation.
    This commit make more clear the access rights
    of a user according to its role and team.
    
    Change-Id: Ie22031584009c2ab749f3e6dbfc1f7fd1d7e12f7

--- dci/server/api/v1/tests.py
+++ dci/server/api/v1/tests.py
@@ -43,7 +43,7 @@ def _verify_existence_and_get_t(t_id):
 
 @api.route('/tests', methods=['POST'])
 @auth2.requires_auth()
-def create_tests(user_info):
+def create_tests(user):
     etag = utils.gen_etag()
     data_json = schemas.test.post(flask.request.json)
     data_json.update({
@@ -64,7 +64,7 @@ def create_tests(user_info):
 
 @api.route('/tests', methods=['GET'])
 @auth2.requires_auth()
-def get_all_tests(user_info):
+def get_all_tests(user):
     args = schemas.args(flask.request.args.to_dict())
 
     query = sqlalchemy.sql.select([models.TESTS])
@@ -92,7 +92,7 @@ def get_all_tests(user_info):
 
 @api.route('/tests/<t_id>', methods=['GET'])
 @auth2.requires_auth()
-def get_test_by_id_or_name(user_info, t_id):
+def get_test_by_id_or_name(user, t_id):
     test = _verify_existence_and_get_t(t_id)
     etag = test['etag']
     test = {'test': dict(test)}
@@ -103,14 +103,14 @@ def get_test_by_id_or_name(user_info, t_id):
 
 @api.route('/tests/<t_id>/jobdefinitions', methods=['GET'])
 @auth2.requires_auth()
-def get_jobdefinitions_by_test(user_info, test_id):
+def get_jobdefinitions_by_test(user, test_id):
     test = _verify_existence_and_get_t(test_id)
     return jobdefinitions.get_all_jobdefinitions(test['id'])
 
 
 @api.route('/tests/<t_id>', methods=['PUT'])
 @auth2.requires_auth()
-def put_test(user_info, t_id):
+def put_test(user, t_id):
     # get If-Match header
     if_match_etag = utils.check_and_get_etag(flask.request.headers)
     data_json = schemas.test.put(flask.request.json)
@@ -137,7 +137,7 @@ def put_test(user_info, t_id):
 
 @api.route('/tests/<t_id>', methods=['DELETE'])
 @auth2.requires_auth()
-def delete_test_by_id_or_name(user_info, t_id):
+def delete_test_by_id_or_name(user, t_id):
     # get If-Match header
     if_match_etag = utils.check_and_get_etag(flask.request.headers)
 
--- dci/server/auth2.py
+++ dci/server/auth2.py
@@ -14,7 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import collections
 import flask
 from functools import wraps
 import json
@@ -26,13 +25,7 @@ from dci.server.db import models_core as models
 from dci.server import dci_config
 
 
-USER = 0
-ADMIN = 1
-SUPER_ADMIN = 2
-
 UNAUTHORIZED = exc.DCIException('Operation not authorized.', status_code=401)
-
-UserInfo = collections.namedtuple('UserInfo', ['team', 'role'])
 
 
 def hash_password(password):
@@ -50,15 +43,9 @@ def build_auth(username, password):
     user = flask.g.db_conn.execute(query_get_user).fetchone()
     if user is None:
         return None, False
-    roles = {'admin': ADMIN, 'user': USER}
-
     user = dict(user)
-    team = user['team_id']
-    role = (SUPER_ADMIN if team == dci_config.TEAM_ADMIN_ID
-            else roles[user['role']])
 
-    return (UserInfo(team, role),
-            pwd_context.verify(password, user.get('password')))
+    return user, pwd_context.verify(password, user.get('password'))
 
 
 def reject():
@@ -74,27 +61,35 @@ def reject():
                           content_type='application/json')
 
 
-def check_super_admin_or_same_team(user_info, team_id):
-    if user_info.role != SUPER_ADMIN and user_info.team != team_id:
+def is_admin(user):
+    return user['team_id'] == dci_config.TEAM_ADMIN_ID
+
+
+def is_super_admin(user):
+    return is_admin(user) and user['role'] == 'admin'
+
+
+def is_admin_or_in_same_team(user, team_id):
+    if not is_admin(user) and user['team_id'] != team_id:
         raise UNAUTHORIZED
 
 
-def requires_auth(level=USER):
+def is_admin_or_admin_user_in_same_team(user, team_id):
+    if (not is_admin(user) and
+            (user['team_id'] != team_id or user['role'] != 'admin')):
+        raise UNAUTHORIZED
+
+
+def requires_auth():
     def wrapper(f):
         @wraps(f)
         def decorated(*args, **kwargs):
             auth = flask.request.authorization
             if not auth:
                 return reject()
-            user_info, is_authenticated = build_auth(auth.username,
-                                                     auth.password)
+            user, is_authenticated = build_auth(auth.username, auth.password)
             if not is_authenticated:
                 return reject()
-
-            if user_info.role < level:
-                raise UNAUTHORIZED
-
-            return f(user_info, *args, **kwargs)
-
+            return f(user, *args, **kwargs)
         return decorated
     return wrapper
