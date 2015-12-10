77a429677a2159d8bdac1f707fe882e8dfdce8f5
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2015-12-10 11:23:10 +0100

    Add 'status' field to job resource
    Currently it's very slow to get the status of all the jobs,
    it's a N+1 select issue because for each job we must first
    get the associated jobstates in order to get the last one.
    
    This commits solve the issue by adding a 'status' field
    inside the job table. After each creation of a new jobstate
    then the associated job status is updated according to the
    new status.
    
    Change-Id: I636816277b4c015646b0dd95413d29fd0a6df722

--- dci/server/api/v1/jobs.py
+++ dci/server/api/v1/jobs.py
@@ -84,7 +84,8 @@ def schedule_jobs(user_info):
          'created_at': datetime.datetime.utcnow().isoformat(),
          'updated_at': datetime.datetime.utcnow().isoformat(),
          'etag': etag,
-         'recheck': values.get('recheck', False)}
+         'recheck': values.get('recheck', False),
+         'status': 'started'}
     )
 
     remoteci_id = values.get('remoteci_id')
