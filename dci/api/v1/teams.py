b319d90367fe8defd9e7abb368182d133aa8205b
Cedric LECOMTE <clecomte@redhat.com>
2016-02-02 12:25:26 -0500

    [WIP] Adding the possibility to audit what users are doing
    Change-Id: I3d247a601cd4e7581a9f18345e8d12b792bfff98

--- dci/api/v1/teams.py
+++ dci/api/v1/teams.py
@@ -24,6 +24,7 @@ from sqlalchemy import sql
 from dci.api.v1 import api
 from dci.api.v1 import remotecis
 from dci.api.v1 import utils as v1_utils
+from dci import audit
 from dci import auth
 from dci.common import exceptions as dci_exc
 from dci.common import schemas
@@ -37,6 +38,7 @@ _T_COLUMNS = v1_utils.get_columns_name_with_objects(_TABLE)
 
 @api.route('/teams', methods=['POST'])
 @auth.requires_auth
+@audit.log
 def create_teams(user):
     values = schemas.team.post(flask.request.json)
 
@@ -66,6 +68,7 @@ def create_teams(user):
 
 @api.route('/teams', methods=['GET'])
 @auth.requires_auth
+@audit.log
 def get_all_teams(user):
     args = schemas.args(flask.request.args.to_dict())
 
