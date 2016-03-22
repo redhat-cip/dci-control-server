d1650a997399609e2c33f444b02f0ca366808334
Yanis Guenane <yguenane@redhat.com>
2016-03-22 13:43:52 +0100

    InfluxDB: Create a new entry per job scheduled
    In order to collect metrics on a job basis, everytime a job is scheduled
    we create a new database within influxdb. A new user is created if
    necessary (ie. one user per tenant). The credentials and the IP/PORT
    setup informations are returned to the agent so the agent can send
    metrics directly to the influxdb instance.
    
    Change-Id: I51d1822004ad6d06a2b398d6642e5a4e81f2bc55

---
+++ scripts/start_influxdb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -xe
+
+DCI_INFLUXDB_DIR=${DCI_INFLUXDB_DIR:-".influxdb_dir"}
+
+# get dci_influxdb_dir absolute path
+DCI_INFLUXDB_DIR="$(cd "$(dirname "$0")/.." && pwd)/$DCI_INFLUXDB_DIR"
+# if the database is already running we do not want to run this script
+[ ! -z "$DISABLE_INFLUXDB_START" ] &&exit 0
+
+# Clean the dir for eventual leftovers
+[ -d "$DCI_INFLUXDB_DIR" ] && rm -rf "$DCI_INFLUXDB_DIR"
+
+# init the database directory and start the process
+sudo systemctl stop influxdb
+mkdir -p $DCI_INFLUXDB_DIR
+cp /etc/influxdb/influxdb.conf $DCI_INFLUXDB_DIR/../influxdb.conf
+sed -i "s#/var/lib/influxdb/data#${DCI_INFLUXDB_DIR}#g" $DCI_INFLUXDB_DIR/../influxdb.conf
+
+influxd -config $DCI_INFLUXDB_DIR/../influxdb.conf
