abe6e367250f22a8f275a5e57b888ea9804b146c
Yanis Guenane <yguenane@redhat.com>
2016-03-22 13:43:52 +0100

    InfluxDB: Create a new entry per job scheduled
    In order to collect metrics on a job basis, everytime a job is scheduled
    we create a new database within influxdb. A new user is created if
    necessary (ie. one user per tenant). The credentials and the IP/PORT
    setup informations are returned to the agent so the agent can send
    metrics directly to the influxdb instance.
    
    Change-Id: I51d1822004ad6d06a2b398d6642e5a4e81f2bc55

--- dci/settings.py
+++ dci/settings.py
@@ -25,6 +25,13 @@ DEBUG = True
 ES_HOST = '127.0.0.1'
 ES_PORT = '9200'
 
+# InfluxDB Connection parameters
+#
+INFLUXDB_HOST = '127.0.0.1'
+INFLUXDB_PORT = '8086'
+INFLUXDB_USER = 'root'
+INFLUXDB_PASS = 'root'
+
 # Database (SQLAlchemy) related parameters
 #
 SQLALCHEMY_DATABASE_URI = 'postgresql://dci:dci@127.0.0.1:5432/dci'
