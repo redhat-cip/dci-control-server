90486ae2f01f111e225e8aaf2628d82d0f9aaaf1
Yanis Guenane <yguenane@redhat.com>
2016-03-22 13:43:52 +0100

    InfluxDB: Create a new entry per job scheduled
    In order to collect metrics on a job basis, everytime a job is scheduled
    we create a new database within influxdb. A new user is created if
    necessary (ie. one user per tenant). The credentials and the IP/PORT
    setup informations are returned to the agent so the agent can send
    metrics directly to the influxdb instance.
    
    Change-Id: I51d1822004ad6d06a2b398d6642e5a4e81f2bc55

--- dci/app.py
+++ dci/app.py
@@ -19,13 +19,18 @@ from dci.api import v1 as api_v1
 from dci.common import exceptions
 from dci.common import utils
 from dci.elasticsearch import engine as es_engine
+from dci.tsdb.drivers import influxdb_driver
 
 import flask
 import logging
-
 from sqlalchemy import exc as sa_exc
 
 from dci import dci_config
+
+
+TSDB_DRIVERS = {
+    'influxdb': influxdb_driver.InfluxDB
+}
 
 
 class DciControlServer(flask.Flask):
@@ -36,6 +41,7 @@ class DciControlServer(flask.Flask):
         self.url_map.strict_slashes = False
         self.engine = dci_config.get_engine(conf)
         self.es_engine = es_engine.DCIESEngine(conf)
+        self.tsdb_engine = TSDB_DRIVERS[conf['TSDB_DRIVER']](conf)
 
     def make_default_options_response(self):
         resp = super(DciControlServer, self).make_default_options_response()
@@ -93,6 +99,7 @@ def create_app(conf):
     def before_request():
         flask.g.db_conn = dci_app.engine.connect()
         flask.g.es_conn = dci_app.es_engine
+        flask.g.tsdb_conn = dci_app.tsdb_engine
 
     @dci_app.teardown_request
     def teardown_request(_):
