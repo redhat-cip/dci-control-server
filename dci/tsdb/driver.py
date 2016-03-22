76d0e0081bb5cd40aa4eb92008707948a9265ddd
Yanis Guenane <yguenane@redhat.com>
2016-03-22 13:43:52 +0100

    InfluxDB: Create a new entry per job scheduled
    In order to collect metrics on a job basis, everytime a job is scheduled
    we create a new database within influxdb. A new user is created if
    necessary (ie. one user per tenant). The credentials and the IP/PORT
    setup informations are returned to the agent so the agent can send
    metrics directly to the influxdb instance.
    
    Change-Id: I51d1822004ad6d06a2b398d6642e5a4e81f2bc55

---
+++ dci/tsdb/driver.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+class BaseTSDB(object):
+
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host', None)
+        self.port = kwargs.get('port', None)
+        self.user = kwargs.get('user', None)
+        self.password = kwargs.get('password', None)
+        self.conn = None
+
+    def create_database(self, name):
+        pass
+
+    def create_user(self, name, password):
+        pass
+
+    def grant_privilege(self, username, databasename):
+        pass
