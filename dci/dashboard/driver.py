f3e5f9a144e375826999f65cc4dc2d4f95022f1d
Yanis Guenane <yguenane@redhat.com>
2016-03-23 09:27:28 +0100

    Grafana: Create a new datasource per job scheduled
    Change-Id: Id66b49bf4adf8edd5fbb1d9ce788537000e4f6b7

---
+++ dci/dashboard/driver.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+class BaseDashboardEngine(object):
+
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host')
+        self.port = kwargs.get('port')
+        self.user = kwargs.get('user')
+        self.password = kwargs.get('password')
+        self.tsdb_host = kwargs.get('tsdb_host')
+        self.tsdb_port = kwargs.get('tsdb_port')
+        self.tsdb_user = kwargs.get('tsdb_user')
+        self.tsdb_password = kwargs.get('tsdb_password')
+        self.url = None
+        self.dashboard = None
+
+    def create_datasource(self, name):
+        pass
+
+    def create_dashboard(self, name):
+        pass
