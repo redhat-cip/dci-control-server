65b2644e750b4e3b27118eb9eeaf1c2560582b59
Yanis Guenane <yguenane@redhat.com>
2016-03-31 10:08:26 +0200

    JUnit: Handle Junit files
    Currently when junit results of a job are submitted they are treated as
    any other file. They're displayed in raw format. Making them not easy to
    read.
    
    The purpose of this review is to serialize JUnit files into python
    object that the API sends back to the UI. The UI is then in charge to
    display them nicely.
    
    Change-Id: I35d953258611389d883547430d6853bec199ae5f

---
+++ dci/api/v1/transformations.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import dci
+import logging
+import lxml.etree as ET
+import os
+import pkg_resources
+
+LOG = logging.getLogger('werkzeug')
+
+
+def junittojson(result):
+
+    xslt = ET.fromstring(
+        pkg_resources.resource_string(dci.__name__,
+                                      os.path.join('data', 'junittojson.xsl')))
+    if not result['content']:
+        result['content'] = '{}'
+    else:
+        try:
+            dom = ET.fromstring(result['content'])
+            transform = ET.XSLT(xslt)
+            result['content'] = str(transform(dom))
+        except ET.XMLSyntaxError as e:
+            result['content'] = '{ "error": "XMLSyntaxError: %s " }' % str(e)
+            LOG.info('transformations.junittojson: XMLSyntaxError %s' % str(e))
+
+    return result
+
+
+def transform(results):
+
+    for result in results:
+        if result['mime'] == 'application/junit':
+            result = junittojson(result)
+
+    return results
