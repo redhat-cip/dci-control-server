41dc583ae19442c46a120d1d213b9a3df126edb6
Cedric LECOMTE <clecomte@redhat.com>
2016-04-18 10:59:57 -0400

    Put ES in async mode
    - Manually index a file for search test
    - Delete the sync way of indexing
    - Create a script for cron to index all files
    
    Change-Id: I897f2156771300e52605a33f7931b62fe486e252

--- tests/conftest.py
+++ tests/conftest.py
@@ -195,26 +195,41 @@ def jobstate_user_id(user, job_user_id):
 
 
 @pytest.fixture
-def file_id(admin, jobstate_id):
+def file_id(admin, jobstate_id, team_admin_id):
     data = {'jobstate_id': jobstate_id,
             'content': 'kikoolol', 'name': 'name'}
     file = admin.post('/api/v1/files', data=data).data
+    data['team_id'] = team_admin_id
+    data['id'] = file['file']['id']
+    conf = dci_config.generate_conf()
+    conn = es_engine.DCIESEngine(conf)
+    conn.index(data)
     return file['file']['id']
 
 
 @pytest.fixture
-def file_user_id(user, jobstate_user_id):
+def file_user_id(user, jobstate_user_id, team_user_id):
     data = {'jobstate_id': jobstate_user_id,
             'content': 'kikoolol', 'name': 'name'}
     file = user.post('/api/v1/files', data=data).data
+    data['team_id'] = team_user_id
+    data['id'] = file['file']['id']
+    conf = dci_config.generate_conf()
+    conn = es_engine.DCIESEngine(conf)
+    conn.index(data)
     return file['file']['id']
 
 
 @pytest.fixture
-def file_job_user_id(user, job_id):
+def file_job_user_id(user, job_id, team_user_id):
     data = {'job_id': job_id,
             'content': 'foobar', 'name': 'name'}
     file = user.post('/api/v1/files', data=data).data
+    data['team_id'] = team_user_id
+    data['id'] = file['file']['id']
+    conf = dci_config.generate_conf()
+    conn = es_engine.DCIESEngine(conf)
+    conn.index(data)
     return file['file']['id']
 
 
