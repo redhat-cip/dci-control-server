7b340660611a316865a40ab4409145d75836555e
Yanis Guenane <yguenane@redhat.com>
2016-07-11 11:18:53 +0200

    jobs: Create a new endpoint /jobs/<j_id>/results
    A new endpoint /jobs/<j_id>/results has been created.
    It aims to return jobs results when they exist.
    
    Output looks like:
    
        {
            "_meta": {
                "count": 1
            },
            "results": [
              {
                "errors": "0",
                "failures": "0",
                "filename": "res_junit.xml",
                "name": "pytest",
                "skips": "1",
                "success": 2,
                "time": "46.050",
                "total": "3"
              }
            ]
        }
    
    Change-Id: I5945c2e11bf9c98f7d27e5b4913f2868e6d13c15

--- tests/conftest.py
+++ tests/conftest.py
@@ -281,6 +281,35 @@ def file_job_user_id(user, job_id, team_user_id):
 
 
 @pytest.fixture
+def file_job_junit_user_id(user, job_id, team_user_id):
+    JUNIT = """
+    <testsuite errors="0" failures="0" name="pytest" skips="1"
+               tests="3" time="46.050">
+    <properties>
+      <property name="x" value="y" />
+      <property name="a" value="b" />
+    </properties>
+    <testcase classname="" file="test-requirements.txt"
+              name="test-requirements.txt" time="0.0109479427338">
+        <skipped message="all tests skipped by +SKIP option"
+                 type="pytest.skip">Skipped for whatever reasons</skipped>
+    </testcase>
+    <testcase classname="tests.test_app" file="tests/test_app.py" line="26"
+              name="test_cors_preflight" time="2.91562318802"/>
+    <testcase classname="tests.test_app" file="tests/test_app.py" line="42"
+              name="test_cors_headers" time="0.574683904648"/> </testsuite>"""
+
+    headers = {'DCI-JOB-ID': job_id,
+               'DCI-NAME': 'name'}
+    file = user.post('/api/v1/files', headers=headers, data=JUNIT).data
+    headers['team_id'] = team_user_id
+    headers['id'] = file['file']['id']
+    conn = es_engine.DCIESEngine(utils.conf)
+    conn.index(headers)
+    return file['file']['id']
+
+
+@pytest.fixture
 def es_clean(request):
     conn = es_engine.DCIESEngine(utils.conf)
     conn.cleanup()
