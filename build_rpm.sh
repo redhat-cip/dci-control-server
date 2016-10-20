2ef3f556081b57bbe70e3a8f530f0f72d632da18
Gonéri Le Bouder <goneri@redhat.com>
2016-10-20 14:21:20 -0400

    rpm refactoring
    - mock: ensure we never pull stuff from pypi by adding pypi.python.org in
      /etc/hosts
    - drop the build dependency on gcc, we don't need that since all our
      dependencies are pulled by yum now
    - avoid a large list of sed calls and use cat << EOF syntax instead
    - use 'python setup.py sdist' to generate our release tarball. This way
      setuptools can validate the structure of the configuration.
    - requirements.txt: relax the versions pining to allow the use of the
      associated RPM
    - drop dci-common, this package was not necessary
    - temporary disable the python3 package because python3-swiftclient is
      not available yet
    - enable gpgcheck for all the repositories and pull the key over HTTPS
    - do not use dci-extras, we don't need to cherry-pick Python packages
      anymore.
    - install the LICENSE file
    
    Change-Id: I9db68725d6c8cd05fa50a9052abed206fd7811dc

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,6 +4,45 @@ PROJ_NAME=dci
 DATE=$(date +%Y%m%d%H%M)
 SHA=$(git rev-parse HEAD | cut -c1-8)
 
+declare -A repo_conf
+repo_conf["fedora-23-x86_64"]='
+[openstack-mitaka]
+name=OpenStack Mitaka Repository
+baseurl=http://mirror.centos.org/centos/7/cloud/$basearch/openstack-mitaka/
+gpgcheck=1
+enabled=1
+gpgkey=https://raw.githubusercontent.com/openstack/puppet-openstack_extras/91fac8eab81d0ad071130887d72338a82c06a7f4/files/RPM-GPG-KEY-CentOS-SIG-Cloud
+'
+repo_conf["fedora-24-x86_64"]=${repo_conf["fedora-23-x86_64"]}
+repo_conf["epel-7-x86_64"]='
+[elasticsearch-2.x]
+name="Elasticsearch repository for 2.x packages"
+baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+gpgcheck=1
+enabled=1
+gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch
+
+[dci]
+name=Distributed CI - CentOS 7
+baseurl=https://packages.distributed-ci.io/repos/current/el/7/x86_64/
+gpgcheck=1
+gpgkey=https://packages.distributed-ci.io/RPM-GPG-KEY-distributedci
+enabled=1
+
+[dci-devel]
+name="Distributed CI - Devel - CentOS 7"
+baseurl=http://packages.distributed-ci.io/repos/development/el/7/x86_64/
+gpgcheck=1
+gpgkey=https://packages.distributed-ci.io/RPM-GPG-KEY-distributedci
+enabled=1
+
+[centos-openstack-mitaka]
+name=CentOS-7 - OpenStack mitaka
+baseurl=http://mirror.centos.org/centos/7/cloud/x86_64/openstack-mitaka/
+gpgcheck=1
+enabled=1
+gpgkey=https://raw.githubusercontent.com/openstack/puppet-openstack_extras/91fac8eab81d0ad071130887d72338a82c06a7f4/files/RPM-GPG-KEY-CentOS-SIG-Cloud
+'
 
 # Configure rpmmacros to enable signing packages
 #
@@ -16,45 +55,35 @@ rm -rf ${HOME}/rpmbuild
 mock --clean
 rpmdev-setuptree
 cp ${PROJ_NAME}.spec ${HOME}/rpmbuild/SPECS/
-git archive HEAD --format=tgz --output=${HOME}/rpmbuild/SOURCES/${PROJ_NAME}-0.0.${DATE}git${SHA}.tgz
+sed -i "s,__version__ = '\(.*\)',__version__ = '0.0.${DATE}git${SHA}'," dci/version.py
+python setup.py sdist
+cp -v dist/* ${HOME}/rpmbuild/SOURCES/
 sed -i "s/VERS/${DATE}git${SHA}/g" ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 
 for arch in fedora-23-x86_64 epel-7-x86_64; do
+    rpath=$(echo ${arch}|sed s,-,/,g|sed 's,epel,el,')
 
     mkdir -p ${HOME}/.mock
-    cp /etc/mock/${arch}.cfg ${HOME}/.mock/${arch}-with-extras.cfg
-
-    if [[ "$arch" == "fedora-23-x86_64" ]]; then
-        RPATH='fedora/23/x86_64'
-    else
-        RPATH='el/7/x86_64'
-        sed -i '$i[elasticsearch-2.x]' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$iname=Elasticsearch repository for 2.x packages"' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$ibaseurl=http://packages.elastic.co/elasticsearch/2.x/centos' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$igpgcheck=0' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$ienabled=1' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$i[dci-extras]' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$iname=Distributed CI - No upstream package - CentOS 7"' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$ibaseurl=http://packages.distributed-ci.io/repos/extras/el/7/x86_64/' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$igpgcheck=0' ${HOME}/.mock/${arch}-with-extras.cfg
-        sed -i '$ienabled=1' ${HOME}/.mock/${arch}-with-extras.cfg
-	sed -i '$i[centos-openstack-liberty]' ${HOME}/.mock/${arch}-with-extras.cfg
-	sed -i '$iname=CentOS-7 - OpenStack liberty' ${HOME}/.mock/${arch}-with-extras.cfg
-	sed -i '$ibaseurl=http://mirror.centos.org/centos/7/cloud/$basearch/openstack-liberty/' ${HOME}/.mock/${arch}-with-extras.cfg
-	sed -i '$igpgcheck=0' ${HOME}/.mock/${arch}-with-extras.cfg
-	sed -i '$ienabled=1' ${HOME}/.mock/${arch}-with-extras.cfg
-    fi
-
-    # NOTE(spredzy) Add signing options
-    #
-    sed -i "\$aconfig_opts['plugin_conf']['sign_enable'] = True" ${HOME}/.mock/${arch}-with-extras.cfg
-    sed -i "\$aconfig_opts['plugin_conf']['sign_opts'] = {}" ${HOME}/.mock/${arch}-with-extras.cfg
-    sed -i "\$aconfig_opts['plugin_conf']['sign_opts']['cmd'] = 'rpmsign'" ${HOME}/.mock/${arch}-with-extras.cfg
-    sed -i "\$aconfig_opts['plugin_conf']['sign_opts']['opts'] = '--addsign %(rpms)s'" ${HOME}/.mock/${arch}-with-extras.cfg
+    head -n -1 /etc/mock/${arch}.cfg > ${HOME}/.mock/${arch}-with-extras.cfg
+    cat <<EOF >> ${HOME}/.mock/${arch}-with-extras.cfg
+${repo_conf[${arch}]}
+"""
+# NOTE(spredzy) Add signing options
+#
+config_opts['plugin_conf']['sign_enable'] = True
+config_opts['plugin_conf']['sign_opts'] = {}
+config_opts['plugin_conf']['sign_opts']['cmd'] = 'rpmsign'
+config_opts['plugin_conf']['sign_opts']['opts'] = '--addsign %(rpms)s'
+config_opts['use_host_resolv'] = False
+config_opts['files']['etc/hosts'] = """
+127.0.0.1 pypi.python.org
+"""
+config_opts['nosync'] = True
+EOF
 
     # Build the RPMs in a clean chroot environment with mock to detect missing
     # BuildRequires lines.
     mkdir -p development
-    mock -r ${HOME}/.mock/${arch}-with-extras.cfg rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
+    mock -r ${HOME}/.mock/${arch}-with-extras.cfg rebuild --resultdir=development/${rpath} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
 done
