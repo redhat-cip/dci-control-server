6ec00aa1dff2af821259c6c0be33c6d9c177ba09
François Charlier <francois.charlier@redhat.com>
2016-11-29 21:40:38 +0100

    [WIP] implement token authentication in the API
    - new column for 'remoteci': 'api_secret'
    - a token/api_secret is created with each new remoteci
    - PUT /v1/remotecis/<r_id>/api_secret resets the token/api_secret
    - a /v1/jobs/test_token endpoint allows to test
    - use sha256 as a hash to compute the digest to be future-proof
    
    Change-Id: I946535128d33baecf2c60f334232dd4fea9b0c16

---
+++ tests/test_token.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dci.common import token
+
+
+def test_gen_token():
+    assert len(token.gen_token()) == 64
+    assert len(token.gen_token(128)) == 128
+    assert token.gen_token() != token.gen_token()
