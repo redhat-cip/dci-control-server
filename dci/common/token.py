06cef4db5bbb5e848a9f0eb8656d97ae1561346b
François Charlier <francois.charlier@redhat.com>
2016-11-29 21:40:38 +0100

    [WIP] implement token authentication in the API
    - new column for 'remoteci': 'api_secret'
    - a token/api_secret is created with each new remoteci
    - PUT /v1/remotecis/<r_id>/api_secret resets the token/api_secret
    - a /v1/jobs/test_token endpoint allows to test
    - use sha256 as a hash to compute the digest to be future-proof
    
    Change-Id: I946535128d33baecf2c60f334232dd4fea9b0c16

---
+++ dci/common/token.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import hashlib
+import hmac
+import random
+import string
+
+
+def gen_secret(length=64):
+    """ Generates a secret of given length
+    """
+    charset = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.SystemRandom().choice(charset)
+                   for _ in range(length))
+
+
+def _digest_request(secret, request):
+    h = hmac.new(secret.encode(), digestmod=hashlib.sha256)
+    h.update(request.url.ode(request.charset or 'utf-8'))
+    if request.data:
+        h.update(h.data.encode(request.charset or 'utf-8'))
+    return h
+
+
+def compare_digest(foo, bar):
+    f = getattr(hmac, 'compare_digest', lambda a, b: a == b)
+    return f(foo, bar)
