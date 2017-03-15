318ec5f22892beeee246650a7bcae3b8ff24bc3d
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-03-15 14:35:54 +0100

    components: handle embeds
    The call
        /topics/<t-id>/components/<c-id>/jobs
    is replaced by
        /components/<c-id>?embed=jobs
    
    The call
        /components/<c-id>?embed=files
    is replaced by
        /components/<c-id>?embed=cfiles
    
    Change-Id: Ib0fffa8675dad083ff33cc2403e30de5e838f6ff

--- tests/api/v1/test_components.py
+++ tests/api/v1/test_components.py
@@ -169,10 +169,10 @@ def test_get_component_by_id_or_name(admin, topic_id):
     assert created_ct['component']['id'] == pc_id
 
 
-def test_get_component_export_control(admin, user, topic_id):
+def test_get_component_export_control(admin, user, topic_user_id):
     data = {'name': 'pname',
             'type': 'gerrit_review',
-            'topic_id': topic_id,
+            'topic_id': topic_user_id,
             'export_control': False
             }
     ncomp = admin.post('/api/v1/components', data=data)
@@ -348,9 +348,9 @@ def test_add_file_to_component(admin, topic_id):
         assert l_file.data['_meta']['count'] == 1
         assert l_file.data['component_files'][0]['component_id'] == ct_1['id']
         cts = admin.get(
-            '/api/v1/components/%s?embed=files' % ct_1['id']).data
-        assert len(cts['component']['files']) == 1
-        assert cts['component']['files'][0]['size'] == 1
+            '/api/v1/components/%s?embed=cfiles' % ct_1['id']).data
+        assert len(cts['component']['cfiles']) == 1
+        assert cts['component']['cfiles'][0]['size'] == 1
 
 
 def test_download_file_from_component(admin, topic_id):
