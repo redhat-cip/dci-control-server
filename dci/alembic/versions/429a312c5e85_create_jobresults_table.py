d22c5167c9acf8081b8d70bce687c0651d356eaa
Guillaume Vincent <gvincent@redhat.com>
2017-03-16 12:22:41 +0100

    [WIP] create model jobresults
    Change-Id: Ifed1ddef23ab28964300c34f0d4f4d09917c696c

---
+++ dci/alembic/versions/429a312c5e85_create_jobresults_table.py
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""create jobresults table
+
+Revision ID: 429a312c5e85
+Revises: 1bb42ff54435
+Create Date: 2017-03-30 07:36:44.830095
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '429a312c5e85'
+down_revision = '1bb42ff54435'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+def upgrade():
+    op.create_table(
+        'tests_results',
+        sa.Column('id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('name', sa.String(length=255), nullable=False),
+        sa.Column('total', sa.Integer(), nullable=True),
+        sa.Column('success', sa.Integer(), nullable=True),
+        sa.Column('skips', sa.Integer(), nullable=True),
+        sa.Column('failures', sa.Integer(), nullable=True),
+        sa.Column('errors', sa.Integer(), nullable=True),
+        sa.Column('time', sa.Integer(), nullable=True),
+        sa.Column('job_id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column('file_id', postgresql.UUID(as_uuid=True), nullable=False),
+        sa.ForeignKeyConstraint(['file_id'], ['files.id'],
+                                ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['job_id'], ['jobs.id'],
+                                ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('tests_results')
