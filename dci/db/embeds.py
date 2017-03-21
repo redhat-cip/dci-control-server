29416ca0e3def3b2b75071a23e4e1a07d306fb54
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-03-21 19:50:10 +0100

    jobdefinitions: handle embeds
    Change-Id: I271b867de2802d8fc162c17027425b3deccecdcd

--- dci/db/embeds.py
+++ dci/db/embeds.py
@@ -52,6 +52,8 @@ JOBSTATE = models.JOBSTATES.alias('jobstate')
 JOBSTATE_JOBS = models.JOBS.alias('jobstate.job')
 JOBSTATEJOBS_WITHOUT_CONFIGURATION = ignore_columns_from_table(JOBSTATE_JOBS, ['configuration'])  # noqa
 
+TOPIC = models.TOPICS.alias('topic')
+
 
 def jobs(root_select=models.JOBS):
     return {
@@ -196,6 +198,15 @@ def files(root_select=models.FILES):
     }
 
 
+def jobdefinitions(root_select=models.JOBDEFINITIONS):
+    return {
+        'topic': [
+            {'right': TOPIC,
+             'onclause': root_select.c.topic_id == TOPIC.c.id}
+        ]
+    }
+
+
 # associate the name table to the object table
 # used for select clause
 EMBED_STRING_TO_OBJECT = {
@@ -221,7 +232,10 @@ EMBED_STRING_TO_OBJECT = {
         'jobstate': JOBSTATE,
         'jobstate.job': JOBSTATEJOBS_WITHOUT_CONFIGURATION,
         'job': JOB_WITHOUT_CONFIGURATION,
-        'team': TEAM}
+        'team': TEAM},
+    'jobdefinitions': {
+        'topic': TOPIC
+    }
 }
 
 
@@ -230,7 +244,8 @@ EMBED_JOINS = {
     'jobs': jobs,
     'remotecis': remotecis,
     'components': components,
-    'files': files
+    'files': files,
+    'jobdefinitions': jobdefinitions
 }
 
 
@@ -250,17 +265,6 @@ def embed(many=False, select=None, where=None,
     :param join: an SQLAlchemy-core Join instance
     """
     return Embed(many, select, where, sort, join)
-
-
-def jobdefinitions():
-    topic = models.TOPICS.alias('topic')
-    return {
-        'topic': embed(
-            select=[topic],
-            where=and_(
-                models.JOBDEFINITIONS.c.topic_id == topic.c.id,
-                topic.c.state != 'archived'
-            ))}
 
 
 def jobstates():
