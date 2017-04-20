591fc0f7cf5874c2963c07aae441453fc8450bb4
Yanis Guenane <yguenane@redhat.com>
2017-04-20 10:33:32 +0200

    Role: Initial commit for the role resource
    This commit introduces the roles resource.
    
    This commit does not implement the usage of the mechanism itself but
    really implement the ground to be able to use it. roles will be
    resources that will hold permissions and that will be attached to users.
    
    Admin of the platform can manage roles.
    
    Change-Id: I45500a2b4f2a3997dcc055ae9d24f869afa01724

---
+++ tests/api/v1/test_roles.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import unicode_literals
+
+
+def test_success_create_role_admin(admin):
+    data = {
+        'name': 'Manager',
+        'label': 'MANAGER',
+        'description': 'A Manager role',
+    }
+
+    result = admin.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 201
+    assert result.data['role']['name'] == data['name']
+    assert result.data['role']['label'] == data['label']
+    assert result.data['role']['description'] == data['description']
+
+
+def test_success_create_role_admin_default_label(admin):
+    data = {
+        'name': 'Manager',
+        'description': 'A Manager role',
+    }
+
+    result = admin.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 201
+    assert result.data['role']['label'] == data['name'].upper()
+
+
+def test_fail_create_role_team_admin(user_admin):
+    data = {
+        'name': 'Manager',
+        'label': 'MANAGER',
+        'description': 'A Manager role',
+    }
+
+    result = user_admin.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 401
+
+
+def test_fail_create_role_user(user):
+    data = {
+        'name': 'Manager',
+        'label': 'MANAGER',
+        'description': 'A Manager role',
+    }
+
+    result = user.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 401
+
+
+def test_success_create_role_correct_payload(admin):
+    data = {
+        'name': 'Manager',
+        'label': 'MANAGER',
+        'description': 'A Manager role',
+    }
+
+    result = admin.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 201
+
+
+def test_fail_ensure_payload_content_is_checked(admin):
+    data = {
+        'description': 'A Manager role',
+    }
+
+    result = admin.post('/api/v1/roles', data=data)
+
+    assert result.status_code == 400
+
+
+def test_fail_create_role_already_exists(admin):
+    data = {
+        'name': 'Manager',
+        'label': 'MANAGER',
+        'description': 'A Manager role',
+    }
+
+    result = admin.post('/api/v1/roles', data=data)
+    assert result.status_code == 201
+    result = admin.post('/api/v1/roles', data=data)
+    assert result.status_code == 409
+
+
+def test_success_update_role(admin, role):
+    role_id = role['id']
+
+    url = '/api/v1/roles/%s' % role_id
+    assert role['name'] == 'Manager'
+
+    result = admin.put(url, data={'name': 'User'},
+                       headers={'If-match': role['etag']})
+    assert result.status_code == 204
+
+    role = admin.get(url).data
+    assert role['role']['name'] == 'User'
+    assert role['role']['description'] == 'A Manager role'
+
+    result = admin.put(url, data={'description': 'new role'},
+                       headers={'If-match': role['role']['etag']})
+    assert result.status_code == 204
+
+    role = admin.get(url).data
+    assert role['role']['name'] == 'User'
+    assert role['role']['description'] == 'new role'
+
+
+def test_fail_update_role_label(admin, role):
+    url = '/api/v1/roles/%s' % role['id']
+
+    result = admin.put(url, data={'label': 'MANAGER_UPDATE'},
+                       headers={'If-match': role['etag']})
+
+    assert result.status_code == 400
+
+
+def test_fail_update_role_unauthorized_fields(admin, role):
+    team_id = {
+        'team_id': 'a265e652-dcbc-4d99-9f74-e60988250403',
+    }
+
+    result = admin.put('/api/v1/roles/%s' % role['id'], data=team_id,
+                       headers={'If-match': role['etag']})
+
+    assert result.status_code == 400
+
+
+def test_success_get_role_by_id(admin, role):
+    result = admin.get('/api/v1/roles/%s' % role['id'])
+
+    assert result.status_code == 200
+    assert result.data['role']['name'] == 'Manager'
+
+
+def test_success_get_all_roles_admin(admin, role):
+    result = admin.get('/api/v1/roles')
+
+    assert result.status_code == 200
+    assert len(result.data['roles']) == 1
+
+
+def test_success_get_all_roles_user(user, role):
+    result = user.get('/api/v1/roles')
+
+    assert result.status_code == 200
+    assert len(result.data['roles']) == 1
+
+
+def test_success_delete_role_admin(admin, role):
+    result = admin.delete('/api/v1/roles/%s' % role['id'],
+                          headers={'If-match': role['etag']})
+
+    assert result.status_code == 204
+
+    result = admin.get('/api/v1/roles')
+
+    assert len(result.data['roles']) == 0
+
+
+def test_fail_delete_role_user(user, role):
+    result = user.delete('/api/v1/roles/%s' % role['id'],
+                         headers={'If-match': role['etag']})
+
+    assert result.status_code == 401
