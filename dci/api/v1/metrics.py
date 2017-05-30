968f34c04e47b53b60f700e561371b7353cba606
Dimitri Savineau <dsavinea@redhat.com>
2017-05-30 14:25:32 -0400

    metrics: Initial commit for metric resource
    The goal of this endpoint is to retrieve information on topics and
    components.
    The metrics endpoint currently gives per component:
      - The delay (in hours) between the puddle import and the first
    job run.
      - The delay (in hours) since the last job run.
    
    Change-Id: Ic1171b8f5197c7b9d2ad9c6ab7b75163a9ee6395

---
+++ dci/api/v1/metrics.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import flask
+from sqlalchemy import sql
+
+from dci.api.v1 import api
+from dci import auth
+from dci.db import models
+
+
+def _get_all_topics():
+    stmt = (sql.select([models.TOPICS.c.id,
+                        models.TOPICS.c.name])
+            .select_from(models.TOPICS)
+            .where(models.TOPICS.c.state == 'active')
+            .order_by(models.TOPICS.c.name))
+    return flask.g.db_conn.execute(stmt).fetchall()
+
+
+def _get_all_components_by_topic(topic_id):
+    stmt = (sql.select([models.COMPONENTS.c.id,
+                        models.COMPONENTS.c.name,
+                        models.COMPONENTS.c.created_at])
+            .select_from(models.COMPONENTS)
+            .where(
+                sql.and_(
+                    models.COMPONENTS.c.topic_id == topic_id,
+                    models.COMPONENTS.c.state == 'active'))
+            .order_by(models.COMPONENTS.c.created_at.asc()))
+    return flask.g.db_conn.execute(stmt).fetchall()
+
+
+def _get_all_jobs_by_component(component_id):
+    stmt = (sql.select([models.JOBS.c.created_at])
+            .select_from(
+                sql.join(
+                    models.JOBS,
+                    models.JOIN_JOBS_COMPONENTS))
+            .where(
+                sql.and_(
+                    models.JOBS.c.state == 'active',
+                    models.JOIN_JOBS_COMPONENTS.c.component_id == component_id))  # noqa
+            .order_by(models.JOBS.c.created_at))
+    return flask.g.db_conn.execute(stmt).fetchall()
+
+
+@api.route('/metrics/topics', methods=['GET'])
+@auth.login_required
+def get_all_metrics(user):
+    if not auth.is_admin(user):
+        raise auth.UNAUTHORIZED
+
+    data = {}
+    topics = _get_all_topics()
+    for t in topics:
+        data[t['name']] = []
+        components = _get_all_components_by_topic(t['id'])
+        for c in components:
+            jobs = _get_all_jobs_by_component(c['id'])
+            if not jobs:
+                first = 'None'
+                last = 'None'
+            else:
+                first = jobs[0]['created_at'] - c['created_at']
+                last = models.datetime.datetime.utcnow() - jobs[-1]['created_at']  # noqa
+                first = str(first.total_seconds() / 3600)
+                last = str(last.total_seconds() / 3600)
+            data[t['name']].append({'component': c['name'],
+                                    'date': c['created_at'],
+                                    'values': [first, last]})
+
+    return flask.jsonify({'topics': data,
+                          '_meta': {'count': len(topics)}})
