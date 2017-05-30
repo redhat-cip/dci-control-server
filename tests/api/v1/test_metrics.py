765c26e64dba1bc8e8c1bf816b9853f477d0bbb6
Dimitri Savineau <dsavinea@redhat.com>
2017-05-30 14:25:32 -0400

    metrics: Initial commit for metric resource
    The goal of this endpoint is to retrieve information on topics and
    components.
    The metrics endpoint currently gives per component the delay in
    seconds between the puddle import and the first job run.
    
    Change-Id: Ic1171b8f5197c7b9d2ad9c6ab7b75163a9ee6395

---
+++ tests/api/v1/test_metrics.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def test_metrics_admin(admin, remoteci_id, team_id):
+    t = admin.post('/api/v1/topics', data={'name': 'foo'}).data
+    t_id = t['topic']['id']
+    jd = admin.post('/api/v1/jobdefinitions',
+                    data={'name': 'jd_foo',
+                          'topic_id': t_id}).data
+    jd_id = jd['jobdefinition']['id']
+    admin.post('/api/v1/components',
+               data={'name': '2017-05-27.1',
+                     'type': 'type_1',
+                     'topic_id': t_id})
+    c = admin.post('/api/v1/components',
+                   data={'name': '2017-06-01.3',
+                         'type': 'type_2',
+                         'topic_id': t_id}).data
+    c_id = c['component']['id']
+    admin.post('/api/v1/topics', data={'name': 'bar'})
+    admin.post('/api/v1/jobs', data={'jobdefinition_id': jd_id,
+                                     'team_id': team_id,
+                                     'remoteci_id': remoteci_id,
+                                     'components': [c_id]})
+    admin.post('/api/v1/jobs', data={'jobdefinition_id': jd_id,
+                                     'team_id': team_id,
+                                     'remoteci_id': remoteci_id,
+                                     'components': [c_id]})
+    res = admin.get('/api/v1/metrics/topics')
+    foo = res.data['topics']['foo']
+    bar = res.data['topics']['bar']
+    assert res.status_code == 200
+    assert len(res.data['topics']) == 2
+    assert len(foo) == 2
+    assert foo[0]['component'] == '2017-05-27.1'
+    assert len(foo[0]['values']) == 0
+    assert foo[1]['component'] == '2017-06-01.3'
+    assert len(foo[1]['values']) == 2
+    assert foo[0]['date'] < foo[1]['date']
+    assert len(bar) == 0
+
+
+def test_metrics_user(user):
+    res = user.get('/api/v1/metrics/topics')
+    assert res.status_code == 401
