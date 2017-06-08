8d18bac56c126e1a7a6c0d43137ddd57a1cd5773
Yanis Guenane <yguenane@redhat.com>
2017-06-08 14:10:54 +0200

    Remove role reference from code base
    Now that the migration to Role Based Access Control is done we can
    remove the role reference that was still in the code base.
    
    Depends-On: I0ee073b0527279f777e978ae51056e4288ae6b79
    Change-Id: Ieee799509f2dad65aa0b0c1bd6124a23adea7528

--- tests/common/utils.py
+++ tests/common/utils.py
@@ -35,7 +35,6 @@ LABEL = 'label', text_type
 ETAG = 'etag', text_type
 DATA = 'data', {'foo': 'bar'}
 PASSWORD = 'password', text_type
-ROLE = 'role', 'admin'
 URL = 'url', 'http://valid.url'
 STATUS = 'status', text_type
 STATE = 'state', 'active'
@@ -61,9 +60,6 @@ INVALID_DATA_ERROR = 'data', schemas.INVALID_JSON
 
 INVALID_PASSWORD = 'password', None
 INVALID_PASSWORD_ERROR = 'password', schemas.INVALID_STRING
-
-INVALID_ROLE = 'role', text_type
-INVALID_ROLE_ERROR = 'role', schemas.INVALID_ROLE
 
 INVALID_URL = 'url', text_type
 INVALID_URL_ERROR = 'url', schemas.INVALID_URL
