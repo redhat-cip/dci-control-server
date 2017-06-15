cf703b84ebace4eea2c2c130725677c84345772c
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-06-15 18:49:36 +0200

    update scheduler
    Change-Id: I495b810b3ace5cd41387195cbf9885039a4d4ce2

--- dci/app.py
+++ dci/app.py
@@ -135,4 +135,19 @@ def create_app(conf):
     # Registering custom encoder
     dci_app.json_encoder = utils.JSONEncoder
 
+    from sqlalchemy import exc
+    from sqlalchemy import event
+    from sqlalchemy.pool import Pool
+
+    @event.listens_for(Pool, "checkout")
+    def ping_connection(dbapi_connection, connection_record, connection_proxy):
+        cursor = dbapi_connection.cursor()
+        try:
+            cursor.execute("SELECT 1")
+        except:
+            # raise DisconnectionError - pool will try
+            # connecting again up to three times before raising.
+            raise exc.DisconnectionError()
+        cursor.close()
+
     return dci_app
