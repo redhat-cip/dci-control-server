c3961d63ec1287b20dbc1cf5e1ab4a9ad1654e65
Yanis Guenane <yguenane@redhat.com>
2017-06-15 10:26:44 +0200

    Schedule: Do not schedule job with an inactive jobdef
    Fixes #37
    
    Change-Id: Ibd7734641e6e34af8744cafa1c9b8d1abf0ab598

--- tests/api/v1/test_jobs.py
+++ tests/api/v1/test_jobs.py
@@ -134,7 +134,7 @@ def test_schedule_jobs_not_active(admin, jobdefinition_id, remoteci_id,
     """
     jd = admin.get('/api/v1/jobdefinitions/%s' % jobdefinition_id).data
     ppt = admin.put('/api/v1/jobdefinitions/%s' % jobdefinition_id,
-                    data={'state': 'inactive'},
+                    data={'state': 'inactive', 'component_types': ['type_1']},
                     headers={'If-match': jd['jobdefinition']['etag']})
     assert ppt.status_code == 204
     job = admin.post('/api/v1/jobs/schedule',
