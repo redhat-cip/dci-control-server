dd267620785c0f560813872778fa234d1ce08cf8
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-06-28 15:43:54 +0200

    fix start_api.sh
    Currently start_api.sh use the endpoint /api/v1 to
    ensure the server is responsive but it's not sufficient
    to ensure the server is correctly linked to the database.
    
    This patch change this behaviour by using the endpoint
    /api/v1/users
    
    Change-Id: I2d880b14c18c77648bfd4e9b02daac9f0562af58

--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -7,10 +7,9 @@ ps aux | awk '!/awk/' | awk '/runtestserver/ {print $2}'| xargs -r kill
 python ./scripts/runtestserver.py provision &
 
 while true; do
-    curl --user admin:admin http://127.0.0.1:5000/api/v1
-    if [ $? -eq 0 ]; then
+    status=$(curl --user admin:admin -s -o /dev/null -w '%{http_code}' http://127.0.0.1:5000/api/v1/users)
+    if [ ${status} -eq 200 ]; then
         break
     fi
     sleep 1
 done
-
