b141eb887ec6ac9312d0567412ce70bc214dd32a
Dimitri Savineau <dsavinea@redhat.com>
2017-06-29 15:19:49 -0400

    scripts: Set timeout to 60s
    To avoid infinite loop we can set a timeout to 60s on the api and
    elasticsearch startup.
    The default value is 60s but could be override.
    
    Change-Id: Id11f14cd99ef6429b863c68ef565c72367972fe5

--- scripts/start_api.sh
+++ scripts/start_api.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 set -x
 
+TIMEOUT=${TIMEOUT:-60}
+
 # kill the running server if it exists
 ps aux | awk '!/awk/' | awk '/runtestserver/ {print $2}'| xargs -r kill
 
 python ./scripts/runtestserver.py provision &
 
-while true; do
+i=1
+while [ $i -lt $TIMEOUT ]; do
     status=$(curl --user admin:admin -s -o /dev/null -w '%{http_code}' http://127.0.0.1:5000/api/v1/users)
     if [ ${status} -eq 200 ]; then
         break
     fi
+    let i++
     sleep 1
 done
+
+if [ $i -eq $TIMEOUT ]; then
+    echo "API not ready in ${TIMEOUT}s"
+    exit 1
+fi
+
+exit 0
--- scripts/start_es.sh
+++ scripts/start_es.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+TIMEOUT=${TIMEOUT:-60}
 DCI_ES_DIR=${DCI_ES_DIR:-".es_dir"}
 
 # get dci_es_dir absolute path
@@ -21,11 +22,20 @@ mkdir -p ${DCI_ES_DIR}/config ${DCI_ES_DIR}/logs ${DCI_ES_DIR}/data
 cp -r /usr/share/elasticsearch/* $DCI_ES_DIR/
 echo "network.host: 0.0.0.0" > ${DCI_ES_DIR}/config/elasticsearch.yml
 ${DCI_ES_DIR}/bin/elasticsearch -d
-while true; do
+
+i=1
+while [ $i -lt $TIMEOUT ]; do
     status=$(curl -s -o /dev/null -w '%{http_code}' http://127.0.0.1:9200/_cluster/health)
     if [ ${status} -eq 200 ]; then
         break
     fi
+    let i++
     sleep 1
 done
 
+if [ $i -eq $TIMEOUT ]; then
+    echo "Elasticsearch not ready in ${TIMEOUT}s"
+    exit 1
+fi
+
+exit 0
