bc5228d80aa8ab7fbea131c82b83bc16ef3d30a3
Yanis Guenane <yguenane@redhat.com>
2017-10-12 12:33:33 +0200

    Feeder: Introduce the new resource
    Change-Id: I2d282d2688e761b76ca77e75cba0cf559e52b8c2

---
+++ tests/api/v1/test_feeders.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import unicode_literals
+
+
+def test_success_create_feeder_authorized_users(admin, product_owner,
+                                                team_product_id):
+    """Test to ensure user with proper permissions can create feeders
+
+       Currently only the role SUPER_ADMIN and PRODUCT_OWNER have such
+       a permission.
+    """
+
+    feeder_from_admin = {
+        'name': 'feeder-from-admin', 'team_id': team_product_id
+    }
+    feeder_from_po = {
+        'name': 'feeder-from-po', 'team_id': team_product_id
+    }
+
+    admin_result = admin.post('/api/v1/feeders', data=feeder_from_admin)
+    po_result = product_owner.post('/api/v1/feeders', data=feeder_from_po)
+
+    assert admin_result.status_code == 201
+    assert admin_result.data['feeder']['name'] == feeder_from_admin['name']
+
+    assert po_result.status_code == 201
+    assert po_result.data['feeder']['name'] == feeder_from_po['name']
+
+
+def test_failure_create_feeder_unauthorized_users(user_admin, user,
+                                                  team_product_id):
+    """Test to ensure user w/o proper permissions can't create feeders
+
+       Currently only the role SUPER_ADMIN and PRODUCT_OWNER have such
+       a permission. So we test with the other roles ADMIN and USER.
+    """
+
+    feeder_from_user_admin = {
+        'name': 'feeder-from-user-admin', 'team_id': team_product_id
+    }
+    feeder_from_user = {
+        'name': 'feeder-from-user', 'team_id': team_product_id
+    }
+
+    user_admin_result = user_admin.post(
+        '/api/v1/feeders', data=feeder_from_user_admin
+    )
+    user_result = user.post('/api/v1/feeders', data=feeder_from_user)
+
+    assert user_admin_result.status_code == 401
+    assert user_result.status_code == 401
+
+
+def test_success_get_feeder_authorized_users(admin, product_owner, feeder):
+    """Test to ensure user with proper permissions can retrieve feeders."""
+
+    admin_result = admin.get('/api/v1/feeders')
+    po_result = product_owner.get('/api/v1/feeders')
+
+    assert admin_result.data['_meta']['count'] == 1
+    assert admin_result.data['feeders'][0]['name'] == 'random-name-feeder'
+
+    assert po_result.data['_meta']['count'] == 1
+    assert po_result.data['feeders'][0]['name'] == 'random-name-feeder'
+
+
+def test_failure_get_feeder_unauthorized_users(user_admin, user, feeder):
+    """Test to ensure user w/o proper permissions can'tretrieve feeders."""
+
+    user_admin_result = user_admin.get('/api/v1/feeders')
+    user_result = user.get('/api/v1/feeders')
+
+    assert user_admin_result.status_code == 401
+    assert user_result.status_code == 401
+
+
+def test_success_delete_feeder_authorized_users(admin, product_owner, feeder,
+                                                team_product_id):
+    """Test to ensure user with proper permissions can delete feeders."""
+
+    feeder_from_po = {
+        'name': 'feeder-from-po', 'team_id': team_product_id
+    }
+
+    po_result = product_owner.post('/api/v1/feeders', data=feeder_from_po)
+    feeder_from_po_id = po_result.data['feeder']['id']
+    feeder_from_po_etag = po_result.headers.get("ETag")
+
+    admin.delete('/api/v1/feeders/%s' % feeder['id'],
+                 headers={'If-match': feeder['etag']})
+    product_owner.delete('/api/v1/feeders/%s' % feeder_from_po_id,
+                         headers={'If-match': feeder_from_po_etag})
+
+    admin_retrieve = admin.get('/api/v1/feeders/%s' % feeder['id'])
+    po_retrieve = product_owner.get('/api/v1/feeders/%s' % feeder_from_po_id)
+
+    assert admin_retrieve.status_code == 404
+    assert po_retrieve.status_code == 404
+
+
+def test_failure_delete_feeder_unauthorized_users(user_admin, user, feeder):
+    """Test to ensure user w/o proper permissions can't delete feeders."""
+
+    user_admin_result = user_admin.delete('/api/v1/feeders/%s' % feeder['id'],
+                                          headers={'If-match': feeder['etag']})
+    user_result = user.delete('/api/v1/feeders/%s' % feeder['id'],
+                              headers={'If-match': feeder['etag']})
+
+    assert user_admin_result.status_code == 401
+    assert user_result.status_code == 401
+
+
+def test_success_put_feeder_authorized_users(admin, product_owner, feeder):
+    """Test to ensure user with proper permissions can update feeders."""
+
+    admin.put('/api/v1/feeders/%s' % feeder['id'], data={'name': 'newname'},
+              headers={'If-match': feeder['etag']})
+
+    admin_result = admin.get('/api/v1/feeders/%s' % feeder['id'])
+    feeder_etag = admin_result.data['feeder']['etag']
+
+    assert admin_result.data['feeder']['name'] == 'newname'
+
+    product_owner.put('/api/v1/feeders/%s' % feeder['id'],
+                      data={'name': 'newname-po'},
+                      headers={'If-match': feeder_etag})
+
+    po_result = product_owner.get('/api/v1/feeders/%s' % feeder['id'])
+
+    assert po_result.data['feeder']['name'] == 'newname-po'
+
+
+def test_failure_put_feeder_unauthorized_users(user_admin, user, feeder):
+    """Test to ensure user w/o proper permissions can't update feeders."""
+
+    user_admin_result = user_admin.put('/api/v1/feeders/%s' % feeder['id'],
+                                       data={'name': 'newname'},
+                                       headers={'If-match': feeder['etag']})
+    user_result = user.put('/api/v1/feeders/%s' % feeder['id'],
+                           data={'name': 'newname'},
+                           headers={'If-match': feeder['etag']})
+
+    assert user_admin_result.status_code == 401
+    assert user_result.status_code == 401
+
+
+def test_success_refresh_secret_feeder_authorized_users(admin, product_owner,
+                                                        feeder):
+    """Test to ensure user with proper permissions can update feeders."""
+
+    original_api_secret = feeder['api_secret']
+    admin.put('/api/v1/feeders/%s/api_secret' % feeder['id'],
+              headers={'If-match': feeder['etag']})
+
+    admin_result = admin.get('/api/v1/feeders/%s' % feeder['id'])
+    feeder_etag = admin_result.data['feeder']['etag']
+
+    assert admin_result.data['feeder']['api_secret']
+    assert admin_result.data['feeder']['api_secret'] != original_api_secret
+
+    original_api_secret = admin_result.data['feeder']['api_secret']
+    product_owner.put('/api/v1/feeders/%s/api_secret' % feeder['id'],
+                      headers={'If-match': feeder_etag})
+
+    po_result = product_owner.get('/api/v1/feeders/%s' % feeder['id'])
+
+    assert po_result.data['feeder']['api_secret']
+    assert po_result.data['feeder']['api_secret'] != original_api_secret
+
+
+def test_failure_refresh_secret_feeder_unauthorized_users(user_admin, user,
+                                                          feeder):
+    """Test to ensure user w/o proper permissions can't update feeders."""
+
+    user_admin_result = user_admin.put(
+        '/api/v1/feeders/%s/api_secret' % feeder['id'],
+        data={'name': 'newname'}, headers={'If-match': feeder['etag']}
+    )
+
+    user_result = user.put(
+        '/api/v1/feeders/%s/api_secret' % feeder['id'],
+        data={'name': 'newname'}, headers={'If-match': feeder['etag']}
+    )
+
+    assert user_admin_result.status_code == 401
+    assert user_result.status_code == 401
