864518352acdef3380ca87885aa09eb1047af0cc
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-10-31 14:56:08 +0100

    QuickFix remoteci parsing
    Change-Id: I624e4ca0af84f2d91399f0b20d472f0b26b37dc3

--- dci/api/v1/jobs.py
+++ dci/api/v1/jobs.py
@@ -334,6 +334,13 @@ def schedule_jobs(user):
     running jobs that were associated with the remoteci. This is because they
     will never be finished.
     """
+
+    # QuickFix
+    remoteci_id = flask.request.json.get('remoteci_id')
+    if remoteci_id is not None and '/' in remoteci_id:
+        remoteci_id = remoteci_id.split('/')[1]
+        flask.request.json['remoteci_id'] = remoteci_id
+
     values = schemas.job_schedule.post(flask.request.json)
 
     values.update({
