c4354c53c5125be07f99b7f1a4c43c5220006aeb
Guillaume Vincent <gvincent@redhat.com>
2017-11-09 12:41:21 +0100

    create hmac mechanism using dciauth
    Change-Id: I1c9ce76f388f47b7efd41012c62ade892d05ee28

--- tests/auth/test_signature_mechanism.py
+++ tests/auth/test_signature_mechanism.py
@@ -19,46 +19,8 @@ import datetime
 import dci.auth_mechanism as authm
 from dci.common import exceptions as dci_exc
 
-import flask
-import mock
 import pytest
 import uuid
-
-
-class MockRequest(object):
-    def __init__(self, auth=None):
-        self.authorization = auth
-
-
-class AuthMock(object):
-    def __init__(self):
-        self.username = 'test'
-        self.password = 'password'
-
-
-def test_basic_auth_mecanism_authenticate_fail_if_no_auth():
-    basic_auth_mecanism = authm.BasicAuthMechanism(MockRequest())
-    with pytest.raises(dci_exc.DCIException):
-        basic_auth_mecanism.authenticate()
-
-
-def test_bam_authenticate_fail_if_not_authenticated():
-    def return_is_authenticated(*args):
-        return {}, False
-
-    basic_auth_mecanism = authm.BasicAuthMechanism(MockRequest(AuthMock()))
-    basic_auth_mecanism.get_user_and_check_auth = return_is_authenticated
-    with pytest.raises(dci_exc.DCIException):
-        basic_auth_mecanism.authenticate()
-
-
-def test_bam_authenticate():
-    def return_is_authenticated(*args):
-        return {}, True
-
-    basic_auth_mecanism = authm.BasicAuthMechanism(MockRequest(AuthMock()))
-    basic_auth_mecanism.get_user_and_check_auth = return_is_authenticated
-    assert basic_auth_mecanism.authenticate()
 
 
 class MockSignedRequest(object):
@@ -176,69 +138,3 @@ def test_sam_authenticate():
     mech.verify_auth_signature = return_is_authenticated
     mech.get_identity = return_get_identity
     mech.authenticate()
-
-
-@mock.patch('jwt.api_jwt.datetime', spec=datetime.datetime)
-def test_sso_auth_verified(m_datetime, admin, app, engine, access_token):
-    m_utcnow = mock.MagicMock()
-    m_utcnow.utctimetuple.return_value = datetime.datetime. \
-        fromtimestamp(1505564918).timetuple()
-    m_datetime.utcnow.return_value = m_utcnow
-    sso_headers = mock.Mock
-    sso_headers.headers = {'Authorization': 'Bearer %s' % access_token}
-    nb_users = len(admin.get('/api/v1/users').data['users'])
-    with app.app_context():
-        flask.g.db_conn = engine.connect()
-        mech = authm.OpenIDCAuth(sso_headers)
-        mech.authenticate()
-        assert mech.identity['team_id'] is None
-        assert mech.identity['name'] == 'dci'
-        assert mech.identity['sso_username'] == 'dci'
-        assert mech.identity['email'] == 'dci@distributed-ci.io'
-        nb_users_after_sso = len(admin.get('/api/v1/users').data['users'])
-        assert (nb_users + 1) == nb_users_after_sso
-
-
-@mock.patch('jwt.api_jwt.datetime', spec=datetime.datetime)
-def test_sso_auth_not_verified(m_datetime, admin, app, engine, access_token):
-    m_utcnow = mock.MagicMock()
-    m_utcnow.utctimetuple.return_value = datetime.datetime. \
-        fromtimestamp(1505564918).timetuple()
-    m_datetime.utcnow.return_value = m_utcnow
-    # corrupt access_token
-    access_token = access_token + 'lol'
-    sso_headers = mock.Mock
-    sso_headers.headers = {'Authorization': 'Bearer %s' % access_token}
-    nb_users = len(admin.get('/api/v1/users').data['users'])
-    with app.app_context():
-        flask.g.db_conn = engine.connect()
-        mech = authm.OpenIDCAuth(sso_headers)
-        with pytest.raises(dci_exc.DCIException):
-            mech.authenticate()
-        assert mech.identity is None
-        nb_users_after_sso = len(admin.get('/api/v1/users').data['users'])
-        assert nb_users == nb_users_after_sso
-
-
-@mock.patch('jwt.api_jwt.datetime', spec=datetime.datetime)
-def test_sso_auth_get_users(m_datetime, user_sso, app, engine):
-    m_utcnow = mock.MagicMock()
-    m_utcnow.utctimetuple.return_value = datetime.datetime. \
-        fromtimestamp(1505564918).timetuple()
-    m_datetime.utcnow.return_value = m_utcnow
-    with app.app_context():
-        flask.g.db_conn = engine.connect()
-        gusers = user_sso.get('/api/v1/users')
-        assert gusers.status_code == 200
-
-
-@mock.patch('jwt.api_jwt.datetime', spec=datetime.datetime)
-def test_sso_auth_get_current_user(m_datetime, user_sso, app, engine):
-    m_utcnow = mock.MagicMock()
-    m_utcnow.utctimetuple.return_value = datetime.datetime. \
-        fromtimestamp(1505564918).timetuple()
-    m_datetime.utcnow.return_value = m_utcnow
-    with app.app_context():
-        flask.g.db_conn = engine.connect()
-        request = user_sso.get('/api/v1/users/me?embed=team,role,remotecis')
-        assert request.status_code == 200
