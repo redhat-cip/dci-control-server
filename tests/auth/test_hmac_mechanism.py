2337d9449750b3c92574b1a55692f683ab80003a
Guillaume Vincent <gvincent@redhat.com>
2018-01-05 11:11:04 +0100

    Use python-dciauth version 2.0.1
    Change-Id: Iab15a58baaa507975c27d0d3b4a2e55cba6f61fc

--- tests/auth/test_hmac_mechanism.py
+++ tests/auth/test_hmac_mechanism.py
@@ -14,32 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from dci import auth_mechanism
-from dci.common import exceptions as dci_exc
-
-import pytest
-
-
-def test_hmac_mechanism_get_client_info():
-    mechanism = auth_mechanism.HmacMechanism({})
-    client_info = mechanism.get_client_info({
-        'DCI-Client-Info': 'remoteci/426fc04f04bf8fdb5831dc37bbb6dcf70f63a37e'
-    })
-    expected_client_info = {
-        'type': 'remoteci',
-        'id': '426fc04f04bf8fdb5831dc37bbb6dcf70f63a37e',
-    }
-    for key in expected_client_info.keys():
-        assert expected_client_info[key] == client_info[key]
-
-
-def test_hmac_mechanism_invalid_header_raise_exception():
-    mechanism = auth_mechanism.HmacMechanism({})
-    with pytest.raises(dci_exc.DCIException):
-        mechanism.get_client_info({
-            'DCI-Invalid-Header': ''
-        })
-
 
 def test_hmac_mechanism_from_outside(remoteci_context):
     jobs_request = remoteci_context.get('/api/v1/jobs')
