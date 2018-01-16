9a4723458aff1e0a13e320974ecb71b8edd62f4a
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2018-01-16 14:35:52 +0100

    [app.py]: remove ES dependency
    If elasticsearch is not reachable then it prevents
    the app to start. This patch change this behavior
    to start the app even if elasticsearch is not reachable.
    
    In elasticsearch/engine.py all the requests are gracefully
    handled with a decorator.
    
    Change-Id: I0d733a7986f3dd396ed8c62ce2fb7568bd41d88f

--- tests/bin/test_dci_essync.py
+++ tests/bin/test_dci_essync.py
@@ -15,19 +15,10 @@
 # under the License.
 
 
-from dci import dci_config
-from dci.elasticsearch import engine
-
 from tests import utils
 
-conf = dci_config.generate_conf()
 
-es_engine = engine.DCIESEngine(es_host=conf['ES_HOST'],
-                               es_port=conf['ES_PORT'],
-                               index='dci', timeout=60)
-
-
-def test_essync_add_files(user, admin, jobstate_user_id):
+def test_essync_add_files(user, admin, jobstate_user_id, es_engine):
     for i in range(5):
         utils.post_file(user, jobstate_user_id,
                         utils.FileDesc('kikoolol', 'content'))
@@ -38,8 +29,8 @@ def test_essync_add_files(user, admin, jobstate_user_id):
            'DCI_LOGIN': 'admin',
            'DCI_PASSWORD': 'admin'}
     status = utils.run_bin('dci-essync', env=env)
-    import pprint
-    pprint.pprint(status.communicate())
+    # import pprint
+    # pprint.pprint(status.communicate())
     status.communicate()
     assert status.returncode == 0
     assert es_engine.get_last_sequence(doc_type='logs') == 5
