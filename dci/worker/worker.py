1ad62a268c2073e53446fa92eda65376faafe8fd
Yanis Guenane <yguenane@redhat.com>
2018-01-19 15:16:15 +0100

    Use authenticated email account for sending notifications
    Up until now, the control-server was relying on the host to act as a
    relay to send notifications. This involved no configuration, simply
    sending the email to the local postfix and ensuring postfix as a relay
    configured.
    
    We did not have the control of the relay server postfix was using.
    Now Distributed-CI has a mail server configured where we can control
    SPF/DKIM and other security related configuration. This change aims
    for the worker to use an authenticated account to send emails.
    
    Change-Id: I2d9a24ab4e07714367b6be0f078b0d04a31c1c61

--- dci/worker/worker.py
+++ dci/worker/worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2016 Red Hat, Inc
+# Copyright (C) 2015-2018 Red Hat, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -15,11 +15,15 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import zmq
 import json
+import os
 import smtplib
+import zmq
 
+from email.MIMEText import MIMEText
 from zmq.eventloop import ioloop, zmqstream
+
+
 ioloop.install()
 
 context = zmq.Context()
@@ -28,28 +32,51 @@ receiver.bind('tcp://0.0.0.0:5557')
 stream = zmqstream.ZMQStream(receiver)
 
 
+def get_email_configuration():
+
+    configuration = {
+        'server': os.getenv('DCI_EMAIL_SERVER', 'mail.distributed-ci.io'),
+        'port': os.getenv('DCI_EMAIL_SERVER_PORT', 587),
+        'account': os.getenv('DCI_EMAIL_ACCOUNT'),
+        'password': os.getenv('DCI_EMAIL_PASSWORD'),
+    }
+
+    if not configuration['account'] or not configuration['password']:
+        configuration = None
+
+    return configuration
+
+
 def mail(mesg):
-    FROM = 'Distributed-CI <dci@distributed-ci.io>'
 
-    SUBJECT = "DCI Status"
+    email_configuration = get_email_configuration()
+    if email_configuration:
+        subject = 'DCI Status'
+        message = "Subject: %s\n"\
+                  "You are receiving this email because of the DCI job %s\n"\
+                  "For the topic : %s on the Remote CI : %s\n"\
+                  "The current status of the job is : %s\n"\
+                  "Message : %s\n\n"\
+                  "For more information : "\
+                  "https://www.distributed-ci.io/#!/jobs/%s/tests"\
+                  % (subject, mesg['job_id'], mesg['topic_id'],
+                     mesg['remoteci_id'], mesg['status'], mesg['mesg'],
+                     mesg['job_id'])
 
-    message = "Subject: %s\n"\
-              "You are receiving this email because of the DCI job %s\n"\
-              "For the topic : %s on the Remote CI : %s\n"\
-              "The current status of the job is : %s\n"\
-              "Message : %s"\
-              "For more information : "\
-              "https://www.distributed-ci.io/#!/jobs/%s/results"\
-              % (SUBJECT, mesg['job_id'], mesg['topic_id'],
-                 mesg['remoteci_id'], mesg['status'], mesg['mesg'],
-                 mesg['job_id'])
+        email = MIMEText(message)
+        email["From"] = 'Distributed-CI Notification <%s>' % \
+            email_configuration['account']
+        email["subject"] = subject
 
-    # Send the mail
-    server = smtplib.SMTP('localhost')
-    for email in mesg['emails']:
-        TO = [email]
-        server.sendmail(FROM, TO, message)
-    server.quit()
+        server = smtplib.SMTP(email_configuration['server'],
+                              email_configuration['port'])
+        server.starttls()
+        server.login(email_configuration['account'],
+                     email_configuration['password'])
+        for contact in mesg['emails']:
+            email['To'] = contact
+            server.sendmail(email['From'], email['To'], email.as_string())
+        server.quit()
 
 
 def loop(msg):
