b158878d623729c762479cb4cc12666856662277
Yanis Guenane <yguenane@redhat.com>
2018-02-06 08:57:55 +0100

    Establish a notification pattern
    Change-Id: I42e61fcc9a4ffc26fe26f85c17eb47fc9bb0faab

---
+++ dci/api/v1/notifications.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import flask
+from dci.db import models
+from sqlalchemy import sql
+
+
+def email(job):
+    _TABLE_URCIS = models.JOIN_USER_REMOTECIS
+
+    query = (sql.select([models.USERS.c.email]).
+             select_from(models.USERS.join(_TABLE_URCIS)).
+             where(_TABLE_URCIS.c.remoteci_id == job['remoteci_id']))
+    result = flask.g.db_conn.execute(query).fetchall()
+
+    emails = [k['email'] for k in result]
+    if emails:
+        msg = {
+            'event': 'notification',
+            'emails': emails,
+            'job_id': str(job['id']),
+            'status': job['status'],
+            'topic_id': str(job['topic_id']),
+            'remoteci_id': str(job['remoteci_id'])
+        }
+        flask.g.sender.send_json(msg)
+
+
+def dlrn(job):
+
+    if job['status'] != 'failure':
+        return
+
+    for component in job['components']:
+        if 'dlrn' in component['data']:
+            if component['data']['dlrn']['commit_hash'] and \
+               component['data']['dlrn']['distro_hash']:
+                msg = {
+                    'event': 'dlrn_publish',
+                    'dlrn': component['data']['dlrn'],
+                    'status': job['status'],
+                    'job_id': job['id'],
+                }
+                flask.g.sender.send_json(msg)
