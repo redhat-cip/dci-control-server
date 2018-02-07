a0c506da3eb2f722a9356e2d954b2d99b0403d67
Guillaume Vincent <gvincent@redhat.com>
2018-02-07 12:38:20 +0100

    New global status endpoint
    create global status per components instead of per topic.
    remove useless metrics endpoint.
    new endpoint /api/v1/global_status
    
    Change-Id: I910a9603bcdfe66d4dcf3668e9d62caecfd683bf

---
+++ tests/api/v1/test_global_status.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def test_global_status(admin, remoteci_context, remoteci_user_id, product):
+    topic = admin.post('/api/v1/topics',
+                       data={
+                           'name': 'OSP12',
+                           'product_id': product['id'],
+                           'component_types': ['puddle']
+                       }).data['topic']
+    admin.post('/api/v1/components',
+               data={
+                   'name': 'RH7-RHOS-12.0 2016-11-12.1',
+                   'type': 'type_1',
+                   'topic_id': topic['id']
+               })
+    component = admin.post('/api/v1/components',
+                           data={
+                               'name': 'RH7-RHOS-12.0 2016-11-13.1',
+                               'type': 'type_1',
+                               'topic_id': topic['id']}).data['component']
+    remoteci_context.post('/api/v1/jobs',
+                          data={'components': [component['id']]})
+    job = remoteci_context.post('/api/v1/jobs',
+                                data={
+                                    'components': [component['id']]
+                                }).data['job']
+    remoteci_context.post('/api/v1/jobstates',
+                          data={'job_id': job['id'], 'status': 'success'})
+    global_status = admin.get('/api/v1/global_status').data
+    stats = global_status['RH7-RHOS-12.0 2016-11-13.1']
+    assert len(stats['jobs']) == 1
+    assert stats['jobs'][0]['status'] == 'success'
+    assert stats['jobs'][0]['remoteci_id'] == remoteci_user_id
+    assert stats['topic_id'] == topic['id']
+    assert stats['topic_name'] == topic['name']
