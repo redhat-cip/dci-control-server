23778aaa30a431c7e0c1d5d7f43cdcc628e05973
Guillaume Vincent <gvincent@redhat.com>
2018-02-07 12:38:20 +0100

    New global status endpoint
    create global status per components instead of per topics.
    remove useless metrics endpoint.
    endpoint /global_status
    
    Change-Id: I910a9603bcdfe66d4dcf3668e9d62caecfd683bf

---
+++ dci/api/v1/global_status.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import flask
+from sqlalchemy import text
+
+from dci.api.v1 import api
+from dci import decorators
+from dci.dci_config import generate_conf, get_engine
+
+
+@api.route('/global_status', methods=['GET'])
+@decorators.login_required
+@decorators.has_role(['SUPER_ADMIN', 'PRODUCT_OWNER'])
+def get_global_status(user):
+    conf = generate_conf()
+    engine = get_engine(conf)
+    latest_components_per_topic = text("""
+SELECT DISTINCT ON (topic_id) topic_id, topics.name as topic_name, components.id, components.name, components.created_at
+FROM components
+JOIN topics ON components.topic_id = topics.id
+ORDER BY topic_id, components.created_at DESC
+""")  # noqa
+    components = engine.execute(latest_components_per_topic)
+    data = {}
+    for component in components:
+        data[component['name']] = dict(component)
+        latest_jobs_per_remoteci_per_component = text("""
+SELECT DISTINCT ON (jobs.remoteci_id) remotecis.name as remoteci_name, teams.external, components.name, components.id, jobs.id, jobs.status, jobs.created_at, jobs.remoteci_id
+FROM jobs
+LEFT JOIN remotecis ON jobs.remoteci_id = remotecis.id
+LEFT JOIN teams ON remotecis.team_id = teams.id
+LEFT JOIN jobs_components ON jobs.id = jobs_components.job_id
+LEFT JOIN components ON components.id = jobs_components.component_id
+WHERE teams.external = '1' AND remotecis.state = 'active' AND components.id = '%s' ORDER BY jobs.remoteci_id, jobs.created_at DESC
+        """ % component['id'])  # noqa
+        jobs = engine.execute(latest_jobs_per_remoteci_per_component)
+        jobs = list(jobs)
+        if len(jobs):
+            data[component['name']]['jobs'] = jobs
+        else:
+            del data[component['name']]
+
+    return flask.jsonify({'data': data})
