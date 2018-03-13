806e56995ceb5c0a2e31435ec529e184041da7b1
Guillaume Vincent <gvincent@redhat.com>
2018-03-13 19:48:45 +0100

    [WIP] Create new endpoint to send certification results
    Change-Id: I87b45b8af082170e5b5554bc9156df5bd27db368

---
+++ dci/api/v1/certification.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import base64
+
+try:
+    from xmlrpclib import ServerProxy
+except ImportError:
+    from xmlrpc.client import ServerProxy
+
+import flask
+
+from dci import dci_config
+from dci.api.v1 import api
+from dci import decorators
+from dci.api.v1.files import get_file_descriptor, get_file_object
+from dci.common import exceptions
+
+
+def build_certification(username, password, certification_id, file_name,
+                        file_content):
+    return {
+        'username': username,
+        'password': password,
+        'id': certification_id,
+        'type': 'certification',
+        'data': base64.b64encode(file_content),
+        'description': 'DCI automatic upload test log',
+        'filename': file_name
+    }
+
+
+@api.route('/certifications', methods=['POST'])
+@decorators.login_required
+@decorators.check_roles
+def send_certification(user):
+    data = flask.request.json
+
+    file_object = get_file_object(data['file_id'])
+    file_descriptor = get_file_descriptor(file_object)
+    file_content = file_descriptor.read()
+
+    certification = build_certification(
+        data['username'],
+        data['password'],
+        data['certification_id'],
+        file_object['name'],
+        file_content
+    )
+    try:
+        conf = dci_config.generate_conf()
+        proxy = ServerProxy(conf['CERTIFICATION_HOST'])
+        proxy.Cert.uploadTestLog(certification)
+        return flask.Response(None, 204, content_type='application/json')
+    except Exception as e:
+        raise exceptions.DCIException('Cannot upload certification result',
+                                      status_code=400)
---
+++ tests/api/v1/test_certification.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import base64
+
+from dci.api.v1 import certification
+
+
+def test_create_certification():
+    with open('tests/data/certification.xml.tar.gz', 'rb') as f:
+        certification_id = '40167'
+        username = 'dci'
+        password = 'dci'
+        file_name = 'certification.xml.tar.gz'
+        file_content = f.read()
+        cert = certification.build_certification(
+            username, password, certification_id, file_name, file_content)
+
+        assert cert['username'] == 'dci'
+        assert cert['password'] == 'dci'
+        assert cert['id'] == '40167'
+        assert cert['type'] == 'certification'
+        assert cert['description'] == 'DCI automatic upload test log'
+        assert cert['filename'] == 'certification.xml.tar.gz'
+
+        base64.decodestring(cert['data'])
