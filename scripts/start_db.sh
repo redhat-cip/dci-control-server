a510d6b790eba5bb4ea0922a323285c4c4f5df8c
Guillaume Vincent <gvincent@redhat.com>
2018-04-06 10:45:29 +0200

    Clean settings files
    The objective is to remove the need of an extra settings file.
    It introduce strong coupling between docker instead of high cohesion.
    Use 12 factor app best pratice.
    See "Store config in the environment" https://12factor.net/config
    
    Change-Id: Ide6a5fcda32981ec506c71c5f9c40e1c91a280e5

--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -2,12 +2,17 @@
 set -xe
 
 DCI_DB_DIR=${DCI_DB_DIR:-".db_dir"}
+DCI_DB_USER=${DCI_DB_USER:-"dci"}
+DCI_DB_PASS=${DCI_DB_PASS:-"dci"}
+DCI_DB_NAME=${DCI_DB_NAME:-"dci"}
 
 # get dci_db_dir absolute path
 DCI_DB_DIR="$(cd "$(dirname "$0")/.." && pwd)/$DCI_DB_DIR"
 
 # if the database is already running we do not want to run this script
 [ ! -z "$DISABLE_DB_START" ] &&exit 0
+
+source /opt/rh/rh-postgresql94/enable
 
 # checks if pg_ctl command exists
 type "pg_ctl"
@@ -17,9 +22,9 @@ pg_ctl status -D "$DCI_DB_DIR" &> /dev/null && pg_ctl stop -D "$DCI_DB_DIR" -m f
 
 [ -d "$DCI_DB_DIR" ] && rm -rf "$DCI_DB_DIR"
 
-OPTIONS="--client-encoding=utf8 --full-page_writes=off \
-         --logging-collector=off --log-destination='stderr'"
-
 # init the database directory and start the process
 pg_ctl initdb -D "$DCI_DB_DIR"
-pg_ctl start -w -D "$DCI_DB_DIR" -o "-k $DCI_DB_DIR -F -h '' $OPTIONS"
+echo "unix_socket_directories = '${DCI_DB_DIR}'" >> $DCI_DB_DIR/postgresql.conf
+pg_ctl start -w -D "$DCI_DB_DIR"
+psql -d postgres -h ${DCI_DB_DIR} -c "CREATE USER ${DCI_DB_USER} WITH SUPERUSER PASSWORD '${DCI_DB_PASS}';"
+createdb -h ${DCI_DB_DIR} -O "${DCI_DB_USER}" "${DCI_DB_NAME}"
