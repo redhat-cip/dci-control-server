f37c6dca91f06be6ed7ac073aaa3ecc4cd04967a
Guillaume Vincent <gvincent@redhat.com>
2018-06-29 17:23:22 +0200

    Trends on topic
    Change-Id: I6d8a1ab93d3d17f7980dc325369c9dd7ff421212

--- dci/api/v1/__init__.py
+++ dci/api/v1/__init__.py
@@ -47,4 +47,5 @@ import dci.api.v1.roles  # noqa
 import dci.api.v1.teams  # noqa
 import dci.api.v1.tests  # noqa
 import dci.api.v1.topics  # noqa
+import dci.api.v1.trends  # noqa
 import dci.api.v1.users  # noqa
