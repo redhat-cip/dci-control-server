a6182082c55c5c2ca34294d08f2577c652e87279
Cedric LECOMTE <clecomte@redhat.com>
2018-07-09 09:33:38 +0200

    Add tags concept to be used by component
    Change-Id: Iae98004db46da897019d1b0fe2811f695a3bb029

---
+++ dci/api/v1/tags.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import flask
+
+from flask import json
+from sqlalchemy import sql
+from dci.api.v1 import utils as v1_utils
+from dci.common import exceptions as dci_exc
+
+
+def create_tag(table, object_id, values):
+    """Create a tag associated to a specific object."""
+
+    with flask.g.db_conn.begin():
+        where_clause = sql.and_(
+            table.c.value == values['value'],
+            table.c.object_id == object_id)
+        query = sql.select([table.c.id]).where(where_clause)
+        if flask.g.db_conn.execute(query).fetchone():
+            raise dci_exc.DCIConflict('Tag already exists', values)
+
+        values.update({'object_id': object_id})
+
+        # create the label/value row
+        query = table.insert().values(**values)
+        flask.g.db_conn.execute(query)
+
+        result = json.dumps({'tag': values})
+        return flask.Response(result, 201,
+                              content_type='application/json')
+
+
+def get_all_tags_from_object(table, object_id):
+    """Get all tags from a specific object."""
+
+    query = (sql.select([table])
+             .where(table.c.object_id == object_id))
+
+    rows = flask.g.db_conn.execute(query)
+
+    res = flask.jsonify({'tags': rows,
+                         '_meta': {'count': rows.rowcount}})
+    res.status_code = 200
+    return res
+
+
+def get_tag_from_object(table, object_id, tag_id):
+    """Get all tags from a specific object."""
+
+    query = (sql.select([table])
+             .where(table.c.id == tag_id))
+
+    rows = flask.g.db_conn.execute(query)
+
+    res = flask.jsonify({'tags': rows,
+                         '_meta': {'count': rows.rowcount}})
+    res.status_code = 200
+    return res
+
+
+def delete_tag(table, object_id, tag_id):
+    """Delete a tag from a specific object."""
+
+    tag_retrieved = v1_utils.verify_existence_and_get(tag_id, table)
+
+    if str(tag_retrieved['object_id']) != object_id:
+        raise dci_exc.DCIDeleteConflict(
+            "Tag '%s' is not associated to object '%s'." % (tag_id, object_id))
+
+    query = table.delete().where(table.c.id == tag_id)
+
+    result = flask.g.db_conn.execute(query)
+
+    if not result.rowcount:
+        raise dci_exc.DCIConflict('Tag deletion conflict', id)
+
+    return flask.Response(None, 204, content_type='application/json')
---
+++ tests/api/v1/test_tags.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from __future__ import unicode_literals
+
+
+def test_attach_tag_to_components(admin, component_id):
+    tag = admin.post('/api/v1/components/%s/tags' % component_id,
+                     data={'value': 'test'}).data
+    result = admin.get('/api/v1/components/%s/tags' % component_id).data
+    assert result['tags'][0]['value'] == tag['tag']['value']
+
+
+def test_delete_tag_on_component(admin, component_id):
+    admin.post('/api/v1/components/%s/tags' % component_id,
+               data={'value': 'test'}).data
+    result = admin.get('/api/v1/components/%s/tags' % component_id).data
+    tag_id = result['tags'][0]['id']
+    admin.delete('/api/v1/components/%s/tags/%s' % (component_id, tag_id))
+    result = admin.get('/api/v1/components/%s/tags' % component_id).data
+    assert len(result['tags']) == 0
