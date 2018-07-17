d3ec2b6be6941e58512a06b39a7de6bd24f21aee
Guillaume Vincent <gvincent@redhat.com>
2018-07-17 12:25:41 +0200

    Speed up dci-dbprovisioning
    Use SQLAchemy to insert fake data in dev database instead of the API.
    The script is 100 times faster.
    Run dbprovisioning when api container starts.
    
    Fixes: https://github.com/redhat-cip/dci-control-server/issues/57
    
    Change-Id: I093455424fc5ab632f056471f0ae501c85479a1e

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DCI_LOGIN='admin' DCI_PASSWORD='admin' python /opt/dci-control-server/bin/dci-dbinit
-
+python /opt/dci-control-server/bin/dci-dbinit
+python /opt/dci-control-server/bin/dci-dbprovisioning
 python /opt/keycloak-provision.py
 
 pubkey=$(python bin/dci-get-pem-ks-key.py http://keycloak:8180 dci-test)
