c9733109ee2305b9f2d4d25789417d3f135ba62c
Guillaume Vincent <gvincent@redhat.com>
2018-07-17 12:25:41 +0200

    Speed up dci-dbprovisioning and refactor init_database script
     * remove the alembic migration inserting READ_ONLY role.
       (initial data are inserted in init_database.py script)
     * rename dci-dbprovisionning into provision_database
     * rewrite dci-dbinit to be reusable by another python script via import
     * run provision_database when api container starts.
     * rewrite provision_database script
       Use SQLAchemy to insert fake data in dev database instead of the API.
       The script is 100 times faster.
       Empty database and run init_database for idempotency.
    
    Change-Id: I093455424fc5ab632f056471f0ae501c85479a1e

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 python bin/wait_for_db
 python bin/init_database
+python bin/provision_database
 python bin/provision_keycloak
 pubkey=$(python bin/generate_keycloak_pem_key http://keycloak:8080 dci-test)
 
