b64d2a11248fc468e0e0ecf3f1fda972166ca96b
Cedric LECOMTE <clecomte@redhat.com>
2018-08-10 11:33:29 +0200

    Add support for files
    Change-Id: I015fc1627a85594fea4f1647b3142904802bc7e9

--- dci/stores/files.py
+++ dci/stores/files.py
@@ -1,40 +1,56 @@
-# -*- coding: utf-8 -*-
+# Copyright 2016 Yanis Guenane <yguenane@redhat.com>
+# Author: Yanis Guenane <yguenane@redhat.com>
 #
-# Copyright (C) 2017 Red Hat, Inc
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dci import stores
+from dci.common import exceptions
+
+import os
+import swiftclient
 
 
-import logging
+class File(stores.Store):
 
-logger = logging.getLogger(__name__)
+    def __init__(self, conf):
+        self.path = conf['path']
+        self.container = conf['container']
 
+    def delete(self, filename):
+        try:
+            os.remove("%s/%s/%s" % (self.path, self.container, filename))
+        except:
+            raise exceptions.StoreExceptions('An error occured while '
+                                             'deleting %s' % filename)
 
-def get_stream_or_content_from_request(request):
-    """Ensure the proper content is uploaded.
+    def get(self, filename):
+        file_path = "%s/%s/%s" % (self.path, self.container, filename)
+        chunk_size = 1024 ** 2
+        with open(file_path, 'rb') as f:
+            for chunk in iter(lambda: f.read(chunk_size) or None, None):
+                yield chunk
 
-    Stream might be already consumed by authentication process.
-    Hence flask.request.stream might not be readable and return improper value.
+    def head(self, filename):
+        file_path = "%s/%s/%s" % (self.path, self.container, filename)
+        file_size = os.path.getsize(file_path)
+        return {'content-length': file_size}
 
-    This methods checks if the stream has already been consumed and if so
-    retrieve the data from flask.request.data where it has been stored.
-    """
+    def upload(self, filename, iterable, pseudo_folder=None,
+               create_container=True):
+        os.makedirs(os.path.join(self.path, self.container, os.path.dirname(filename)))
+        file_path = "%s/%s/%s" % (self.path, self.container, filename)
 
-    if request.stream.tell():
-        logger.info('Request stream already consumed. '
-                    'Storing file content using in-memory data.')
-        return request.data
-
-    else:
-        logger.info('Storing file content using request stream.')
-        return request.stream
+        with open(file_path, 'wb') as f:
+            chunk_size = 4096
+            for chunk in iter(lambda: iterable(chunk_size) or None, None):
+                f.write(chunk)
