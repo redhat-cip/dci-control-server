739b04d174bc9220df421d9f8d9d30605514f320
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2018-09-19 13:04:47 +0200

    remove permissions
    Change-Id: I54b100808126c48cd3df55807dd65c1e0a6148c6

---
+++ dci/alembic/versions/b71a4139c14f_removepermissions.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2018 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""RemovePermissions
+
+Revision ID: b71a4139c14f
+Revises: bcd903a35145
+Create Date: 2018-09-19 13:07:20.467918
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b71a4139c14f'
+down_revision = 'bcd903a35145'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+
+
+def upgrade():
+    op.drop_table('roles_permissions')
+    op.drop_table('permissions')
+
+
+def downgrade():
+    pass
