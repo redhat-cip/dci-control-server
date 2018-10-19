d194f6f0fcfe20878956f2c5d6bd6e90db6b20a1
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2018-10-19 15:40:14 +0200

    [store]: remove store files on purge
     - purge on files will be removed from the
       database and the storage as well
     - purge on component's files will be removed
       from the database and the storage as well
     - purge on jobs will remove files from
       the storage as well
    
    Change-Id: Ic48ccd4c2c5a27d8894c07a4f56993e2bba673cc

--- tests/api/v1/test_files.py
+++ tests/api/v1/test_files.py
@@ -19,16 +19,19 @@ from __future__ import unicode_literals
 import base64
 
 import flask
+import mock
+import pytest
 import six
 
+from dci import dci_config
 from dci.api.v1 import files
 from dci.api.v1.files import get_file_info_from_headers
+from dci.common import exceptions as dci_exc
+from dci.stores import files_utils
 from tests import data as tests_data
 import tests.utils as t_utils
 
 import collections
-
-SWIFT = 'dci.stores.swift.Swift'
 
 FileDesc = collections.namedtuple('FileDesc', ['name', 'content'])
 
@@ -397,3 +400,58 @@ def test_get_previous_job_in_topic(app, user, remoteci_context,
         flask.g.db_conn = engine.connect()
         test_prev_job_id = str(files.get_previous_job_in_topic(new_job)['id'])
         assert prev_job_id == test_prev_job_id
+
+
+def test_purge(app, admin, user, jobstate_user_id, team_user_id, job_user_id):
+    # create two files and archive them
+    file_id1 = t_utils.post_file(user, jobstate_user_id,
+                                 FileDesc('kikoolol', 'content'))
+    user.delete('/api/v1/files/%s' % file_id1)
+    file_id2 = t_utils.post_file(user, jobstate_user_id,
+                                 FileDesc('kikoolol2', 'content2'))
+    user.delete('/api/v1/files/%s' % file_id2)
+
+    to_purge = admin.get('/api/v1/files/purge').data
+    assert len(to_purge['files']) == 2
+    admin.post('/api/v1/files/purge')
+    path1 = files_utils.build_file_path(team_user_id, job_user_id, file_id1)
+    store = dci_config.get_store('files')
+    # the purge removed the file from the backend, get() must raise exception
+    with pytest.raises(dci_exc.StoreExceptions):
+        store.get(path1)
+    path2 = files_utils.build_file_path(team_user_id, job_user_id, file_id2)
+    with pytest.raises(dci_exc.StoreExceptions):
+        store.get(path2)
+    to_purge = admin.get('/api/v1/files/purge').data
+    assert len(to_purge['files']) == 0
+
+
+def test_purge_failure(app, admin, user, jobstate_user_id, job_user_id,
+                       team_user_id):
+    # create two files and archive them
+    file_id1 = t_utils.post_file(user, jobstate_user_id,
+                                 FileDesc('kikoolol', 'content'))
+    user.delete('/api/v1/files/%s' % file_id1)
+    file_id2 = t_utils.post_file(user, jobstate_user_id,
+                                 FileDesc('kikoolol2', 'content2'))
+    user.delete('/api/v1/files/%s' % file_id2)
+
+    to_purge = admin.get('/api/v1/files/purge').data
+    assert len(to_purge['files']) == 2
+
+    # purge will fail
+    with mock.patch('dci.stores.filesystem.FileSystem.delete') as mock_delete:
+        mock_delete.side_effect = dci_exc.StoreExceptions('error')
+        purge_res = admin.post('/api/v1/files/purge')
+        assert purge_res.status_code == 400
+        path1 = files_utils.build_file_path(team_user_id,
+                                            job_user_id,
+                                            file_id1)
+        path2 = files_utils.build_file_path(team_user_id,
+                                            job_user_id,
+                                            file_id2)
+        store = dci_config.get_store('files')
+        store.get(path1)
+        store.get(path2)
+    to_purge = admin.get('/api/v1/files/purge').data
+    assert len(to_purge['files']) == 2
