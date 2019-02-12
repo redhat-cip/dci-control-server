abcd969f2f70a2ce5a62d0909a4f5bc7124b8436
Haikel Guemar <hguemar@fedoraproject.org>
2019-02-12 13:33:22 +0100

    Replace pyzmq with PoolExecutor
    ZMQ sockets are replaced by standard library PoolExecutor.
    A new class dci.worker.worker.Executor is introduced to wrap the PoolExecutor
    and all the events processing code. Its main purpose is to avoid
    circular imports between modules dci.api.v1.notifications and
    dci.worker.worker.
    
    - Remove pyzmq
    - Use concurrent.futures.PoolExecutor
    - PoolExecutor type (process, thread) is configurable
    - Same with max workers
    - Python 2.7: use futures backport instead, ThreadPoolExecutor is default
    - Use six in worker.py for python 3 compatibility
    
    Change-Id: I4104d15c59c50ce14f60e566c7121315b2005402
    Depends-On: I5163c520293f2f43cb67b7f5cee4c2f6741cbbb4

--- dci/api/v1/notifications.py
+++ dci/api/v1/notifications.py
@@ -111,4 +111,5 @@ def dispatcher(job):
         events.append(dlrn_event)
 
     if events:
-        flask.g.sender.send_json(events)
+        pass
+        #flask.g.executor.process_events(events)
