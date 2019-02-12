de4a32132e52e3563d1ac95c85371df5bcea19da
Haikel Guemar <hguemar@fedoraproject.org>
2019-02-12 17:08:47 +0100

    Tox: minor change to run on Fedora
    Use base OS postgres instead of SCL.
    
    Change-Id: I458bdfd5fffccb27222276be63caa0ea9c208ecb

--- scripts/start_db.sh
+++ scripts/start_db.sh
@@ -12,7 +12,11 @@ DCI_DB_DIR="$(cd "$(dirname "$0")/.." && pwd)/$DCI_DB_DIR"
 # if the database is already running we do not want to run this script
 [ ! -z "$DISABLE_DB_START" ] &&exit 0
 
-source /opt/rh/rh-postgresql94/enable
+# Check distro, use base OS postgres on Fedora, SCL on RHEL/CentOS
+DISTRO=$(awk -F "=" '/^ID/ { print $2 }' /etc/os-release)
+if [[ $DISTRO != fedora ]]; then
+    source /opt/rh/rh-postgresql94/enable
+fi
 
 # checks if pg_ctl command exists
 type "pg_ctl"
