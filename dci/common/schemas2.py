9defb638636a0d640256db4809534e3f87af3b36
Guillaume Vincent <gvincent@redhat.com>
2019-05-14 07:39:01 +0200

    Use jsonschema on analytics endpoint
    Change-Id: I2831fc1c92bcea8df8beb1600ebba4665b86f443

--- dci/common/schemas2.py
+++ dci/common/schemas2.py
@@ -152,6 +152,25 @@ def check_and_get_args(raw_args):
 
 ###############################################################################
 #                                                                             #
+#                              Analytics schema                               #
+#                                                                             #
+###############################################################################
+analytic_properties = {
+    "name": Properties.string,
+    "type": Properties.string,
+    "url": with_default(Properties.url, None),
+    "data": Properties.json,
+}
+create_analytic_schema = {
+    "type": "object",
+    "properties": analytic_properties,
+    "required": ["name", "type"],
+    "additionalProperties": False,
+}
+update_analytic_schema = {"type": "object", "properties": analytic_properties}
+
+###############################################################################
+#                                                                             #
 #                                 Tag schema                                  #
 #                                                                             #
 ###############################################################################
