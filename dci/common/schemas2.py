199f966a48425dbdb29bc4ea3882a72bf81315f1
Guillaume Vincent <gvincent@redhat.com>
2019-05-14 07:32:52 +0200

    Replace voluptuous with jsonschema
    In order to improve maintenability of the dci-control-server,
    we are going to remove voluptuous with json schema.
    
    This is the first commit adding required files to edit every endpoint.
    
    Change-Id: Id156fbae2092abeb014243f69bf89207ca7b1605

---
+++ dci/common/schemas2.py
@@ -0,0 +1,133 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dci.common.exceptions import DCIException
+from dci.common.args import parse_args
+from jsonschema import validators, FormatChecker, Draft4Validator
+from jsonschema.exceptions import ValidationError
+
+
+uuid_pattern = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+url_pattern = "^https?://"
+integer_pattern = "^([+-]?[1-9]\d*|0)$"
+positive_integer_pattern = "^[1-9]\d*$"
+positive_or_null_integer_pattern = "^\d+$"
+
+
+class Properties(object):
+    string = {"type": "string"}
+    uuid = {"type": "string", "pattern": uuid_pattern}
+    email = {"type": "string", "format": "email"}
+    url = {"type": "string", "format": "uri", "pattern": url_pattern}
+    json = {"type": "object"}
+    array = {"type": "array"}
+    integer = {"type": "integer"}
+    boolean = {"type": "boolean"}
+    positive_integer = {"type": "integer", "minimum": 1}
+    positive_or_null_integer = {"type": "integer", "minimum": 0}
+    string_integer = {"type": "string", "pattern": integer_pattern}
+    positive_string_integer = {
+        "type": "string",
+        "pattern": positive_integer_pattern
+    }
+    positive_or_null_string_integer = {
+        "type": "string",
+        "pattern": positive_or_null_integer_pattern
+    }
+    key_value_csv = {"type": "string", "is_key_value_csv": True}
+
+    @staticmethod
+    def enum(accepted_values):
+        return {
+            "type": "string",
+            "enum": accepted_values
+        }
+
+custom_error_messages = {
+    uuid_pattern: "is not a valid 'uuid'",
+    url_pattern: "is not a valid 'url'",
+    integer_pattern: "is not an integer",
+    positive_integer_pattern: "is not a positive integer",
+    positive_or_null_integer_pattern: "is not a positive or null integer"
+}
+
+
+def _is_key_value_csv(validator, value, instance, schema):
+    for element in instance.split(","):
+        if len(element.split(":")) != 2:
+            yield ValidationError("'%s' is not a 'key value csv'" % instance)
+
+
+all_validators = dict(Draft4Validator.VALIDATORS)
+all_validators["is_key_value_csv"] = _is_key_value_csv
+
+DCIValidator = validators.create(
+    meta_schema=Draft4Validator.META_SCHEMA, validators=all_validators
+)
+
+
+def _get_error_message(error):
+    if error.validator == "pattern":
+        property_name = error.path[-1]
+        value = error.instance
+        msg = custom_error_messages.get(error.validator_value, error.message)
+        return "%s: '%s' %s" % (property_name, value, msg)
+
+    if (
+        error.validator == "type" and
+        error.validator_value in ["string", "array", "integer"]
+    ) or (
+        error.validator in ["minimum", "is_key_value_csv"]
+    ):
+        property_name = error.path[-1]
+        return "%s: %s" % (property_name, error.message)
+
+    return error.message
+
+
+def check_json_is_valid(schema, json):
+    v = DCIValidator(schema, format_checker=FormatChecker())
+    errors = []
+    for error in sorted(v.iter_errors(json), key=str):
+        errors.append(_get_error_message(error))
+    if len(errors):
+        raise DCIException("Request malformed", {"errors": errors})
+
+###############################################################################
+#                                                                             #
+#                                 Args schema                                 #
+#                                                                             #
+###############################################################################
+args_schema = {
+    "type": "object",
+    "properties": {
+        "limit": Properties.positive_string_integer,
+        "offset": Properties.positive_or_null_string_integer,
+        "sort": Properties.string,
+        "where": Properties.key_value_csv,
+        "embed": Properties.string,
+    },
+    "dependencies": {
+        "limit": {"required": ["offset"]},
+        "offset": {"required": ["limit"]}
+    },
+    "additionalProperties": False,
+}
+
+
+def check_and_get_args(raw_args):
+    check_json_is_valid(args_schema, raw_args)
+    return parse_args(raw_args)
