6ed625778c6e1036883cfba1794a01b1283024d6
Guillaume Vincent <gvincent@redhat.com>
2019-05-17 09:14:33 +0200

    Use jsonschema on feeder controller
    Change-Id: I2f506ef4389fddc83db89dd856be7da861e895b6

--- dci/common/schemas2.py
+++ dci/common/schemas2.py
@@ -286,3 +286,23 @@ update_current_user_schema = {
         "timezone": Properties.string,
     },
 }
+
+###############################################################################
+#                                                                             #
+#                                Feeder schema                                #
+#                                                                             #
+###############################################################################
+create_feeder_properties = {
+    "name": Properties.string,
+    "team_id": Properties.uuid,
+    "state": with_default(Properties.enum(valid_resource_states), "active"),
+    "data": Properties.json,
+}
+create_feeder_schema = {
+    "type": "object",
+    "properties": create_feeder_properties,
+    "required": ["name", "team_id"],
+    "additionalProperties": False,
+}
+
+update_feeder_schema = {"type": "object", "properties": create_feeder_properties}
