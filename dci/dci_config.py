6dd0bb2ae81cb50782852ce76d1eb25d6d175f27
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2019-06-10 03:57:19 +0200

    [SSO internal]: adding dedicated team for RO users.
    Add the dedicated 'internal' team for SSO RO users.
    
    Change-Id: Ida032af985f1a9012745115566a10ce8cf66716c
    Depends-On: Ic965ed9c558ba57bbd8f684d1e7615fb6c83af86

--- dci/dci_config.py
+++ dci/dci_config.py
@@ -63,17 +63,29 @@ def get_store(container):
 
 
 def sanity_check(conf):
+    db_conn = get_engine(conf).connect()
+    # get the admin team id
     query_team_admin_id = sqlalchemy.sql.select([models.TEAMS]).where(
         models.TEAMS.c.name == 'admin')
-
-    db_conn = get_engine(conf).connect()
     row = db_conn.execute(query_team_admin_id).fetchone()
-    db_conn.close()
 
     if row is None:
         print("Admin team not found. Please init the database"
               " with the 'admin' team and 'admin' user.")
         sys.exit(1)
+    team_admin_id = row.id
 
-    # return the team admin id
-    return row.id
+    # get the internal team id
+    query_team_internal_id = sqlalchemy.sql.select([models.TEAMS]).where(
+        models.TEAMS.c.name == 'internal')
+    row = db_conn.execute(query_team_internal_id).fetchone()
+
+    if row is None:
+        print("Internal team not found. Please init the database"
+              " with the 'internal' team.")
+        sys.exit(1)
+    team_internal_id = row.id
+
+    db_conn.close()
+
+    return team_admin_id, team_internal_id
