3d1e5084f6d46a2e3ee5891d7f4cacc134671d35
Guillaume Vincent <gvincent@redhat.com>
2019-07-05 09:15:01 +0200

    Remove table users teams roles
    Change-Id: I93ea6b8fc741da6d73d457c5e61e6f81ec41f690
    Depends-On: I8c4708281c1ef6a967423533af71f4fcb32f457d

--- dci/db/models.py
+++ dci/db/models.py
@@ -476,18 +476,6 @@ JOIN_USERS_TEAMS = sa.Table(
     sa.UniqueConstraint('user_id', 'team_id', name='users_teams_key')
 )
 
-JOIN_USERS_TEAMS_ROLES = sa.Table(
-    'users_teams_roles', metadata,
-    sa.Column('user_id', pg.UUID(as_uuid=True),
-              sa.ForeignKey('users.id', ondelete='CASCADE'),
-              nullable=False),
-    sa.Column('team_id', pg.UUID(as_uuid=True),
-              sa.ForeignKey('teams.id', ondelete='CASCADE'),
-              nullable=True),
-    sa.Column('role', sa.String(255), default='USER', nullable=False),
-    sa.UniqueConstraint('user_id', 'team_id', name='users_teams_roles_key')
-)
-
 JOIN_USER_REMOTECIS = sa.Table(
     'user_remotecis', metadata,
     sa.Column('user_id', pg.UUID(as_uuid=True),
