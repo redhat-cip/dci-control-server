b5f69df98c30428e7c09f29477d55119b8b4ed9a
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2019-08-01 17:03:01 +0200

    jobs: add duration
    the duration is in seconds. It's calculated from the last
    jobstate's created_at field minus the job's created_at field.
    
    Also update bin/dci-dbprovisioning.
    
    Change-Id: Ifad5b04d30e1dece3f96e7163de34e446fef0300

---
+++ dci/alembic/versions/49363052bd7d_add_duration_to_jobs.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2019 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""add duration to jobs
+
+Revision ID: 49363052bd7d
+Revises: 5ad9b5342acf
+Create Date: 2019-07-31 15:44:52.724198
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '49363052bd7d'
+down_revision = '5ad9b5342acf'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('jobs',
+                  sa.Column('duration', sa.Integer))
+
+
+def downgrade():
+    pass
