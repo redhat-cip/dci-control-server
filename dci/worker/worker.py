fe5d56da0f12ead8a596a472fb76ba7b166623a0
François Charlier <francois.charlier@redhat.com>
2020-07-08 16:23:09 +0200

    Add some very light logging for the worker
    Change-Id: Ib0f3bd0a0025620c15df416abd60d16642292933

--- dci/worker/worker.py
+++ dci/worker/worker.py
@@ -19,6 +19,7 @@ from dci.api.v1 import notifications
 from dci.worker.umb import send_event_on_umb
 
 import json
+import logging
 import os
 import smtplib
 import requests
@@ -28,6 +29,9 @@ import zmq
 from email.MIMEText import MIMEText
 from zmq.eventloop import ioloop, zmqstream
 
+
+logging.getLogger().basicConfig(level="DEBUG")
+logger = logging.getLogger(__name__)
 
 ioloop.install()
 
@@ -126,13 +130,20 @@ def loop(msg):
     try:
         events = json.loads(msg[0])
         for event in events:
-            if event['event'] == 'notification':
-                send_mail(event)
-            elif event['event'] == 'dlrn_publish':
-                dlrn_publish(event)
-            elif event['event'] == 'job_finished':
-                send_event_on_umb(event)
-    except:
+            try:
+                if event['event'] == 'notification':
+                    send_mail(event)
+                elif event['event'] == 'dlrn_publish':
+                    dlrn_publish(event)
+                elif event['event'] == 'job_finished':
+                    send_event_on_umb(event)
+            except Exception:
+                logger.exception(
+                    msg="An error has occurred while processing an event: %s" % event
+                )
+                pass
+    except Exception:
+        logger.exception(msg="An error has occurred processing events.")
         pass
 
 
