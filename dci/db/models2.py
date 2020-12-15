ae513aa852fdf19f34946e92f137698142c808ae
ylamgarchal <yassine.lamgarchal@redhat.com>
2020-12-15 01:49:11 +0100

    users.py: use ORM, remove embeds and QueryBuilder
    This patch removes the use of embeds and the QueryBuilder
    class with the following approach:
    
      - use Sqlalchemy ORM layer instead of the core one
      - a new model has been added in models2.py with mapped classes
      - when getting users do the join to teams and remotecis,
        this way the embed doesn't matter
      - the tests has not been modified to have backward compatibility
    
    Change-Id: I1dbd80f1f424ae7112c3aa6635cb66660db4da7d

---
+++ dci/db/models2.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from dci.common import signature
+from dci.db import declarative as dci_declarative
+
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import orm as sa_orm
+import sqlalchemy_utils as sa_utils
+
+Base = declarative_base()
+
+JOB_STATUSES = ['new', 'pre-run', 'running', 'post-run',
+                'success', 'failure', 'killed', 'error']
+STATUSES = sa.Enum(*JOB_STATUSES, name='statuses')
+FINAL_STATUSES = ['success', 'failure', 'error']
+FINAL_FAILURE_STATUSES = ['failure', 'error']
+FINAL_STATUSES_ENUM = sa.Enum(*FINAL_STATUSES, name='final_statuses')
+
+RESOURCE_STATES = ['active', 'inactive', 'archived']
+STATES = sa.Enum(*RESOURCE_STATES, name='states')
+
+ISSUE_TRACKERS = ['github', 'bugzilla']
+TRACKERS = sa.Enum(*ISSUE_TRACKERS, name='trackers')
+
+
+USERS_TEAMS = sa.Table(
+    'users_teams', Base.metadata,
+    sa.Column('user_id', pg.UUID(as_uuid=True),
+              sa.ForeignKey('users.id', ondelete='CASCADE'),
+              nullable=False),
+    sa.Column('team_id', pg.UUID(as_uuid=True),
+              sa.ForeignKey('teams.id', ondelete='CASCADE'),
+              nullable=True),
+    sa.UniqueConstraint('user_id', 'team_id', name='users_teams_key')
+)
+
+USER_REMOTECIS = sa.Table(
+    'user_remotecis', Base.metadata,
+    sa.Column('user_id', pg.UUID(as_uuid=True),
+              sa.ForeignKey('users.id', ondelete='CASCADE'),
+              nullable=False, primary_key=True),
+    sa.Column('remoteci_id', pg.UUID(as_uuid=True),
+              sa.ForeignKey('remotecis.id', ondelete='CASCADE'),
+              nullable=False, primary_key=True)
+)
+
+
+class User(dci_declarative.Mixin, Base):
+    __tablename__ = 'users'
+
+    name = sa.Column(sa.String(255), nullable=False, unique=True)
+    sso_username = sa.Column(sa.String(255), nullable=True, unique=True)
+    fullname = sa.Column(sa.String(255), nullable=False)
+    email = sa.Column(sa.String(255), nullable=False, unique=True)
+    password = sa.Column(sa.Text, nullable=True)
+    timezone = sa.Column(sa.String(255), nullable=False, default='UTC')
+    state = sa.Column(STATES, default='active')
+    team = sa_orm.relationship('Team', secondary=USERS_TEAMS, back_populates='users')
+    remotecis = sa_orm.relationship('Remoteci', secondary=USER_REMOTECIS, back_populates='users')
+
+
+class Team(dci_declarative.Mixin, Base):
+    __tablename__ = 'teams'
+    __table_args__ = (sa.UniqueConstraint('name', name='teams_name_key'),)
+
+    name = sa.Column(sa.String(255), nullable=False)
+    # https://en.wikipedia.org/wiki/ISO_3166-1 Alpha-2 code
+    country = sa.Column(sa.String(255), nullable=True)
+    state = sa.Column(STATES, default='active')
+    external = sa.Column(sa.BOOLEAN, default=True)
+    users = sa_orm.relationship('User', secondary=USERS_TEAMS, back_populates='team')
+
+
+class Remoteci(dci_declarative.Mixin, Base):
+    __tablename__ = 'remotecis'
+    __table_args__ = (sa.Index('remotecis_team_id_idx', 'team_id'),
+                      sa.UniqueConstraint('name', 'team_id', name='remotecis_name_team_id_key'))
+
+    name = sa.Column('name', sa.String(255))
+    data = sa.Column('data', sa_utils.JSONType)
+    api_secret = sa.Column('api_secret', sa.String(64), default=signature.gen_secret)
+    team_id = sa.Column('team_id', pg.UUID(as_uuid=True), sa.ForeignKey('teams.id', ondelete='CASCADE'), nullable=False)
+    public = sa.Column('public', sa.BOOLEAN, default=False)
+    cert_fp = sa.Column('cert_fp', sa.String(255))
+    state = sa.Column('state', STATES, default='active')
+    users = sa_orm.relationship('User', secondary=USER_REMOTECIS, back_populates='remotecis')
