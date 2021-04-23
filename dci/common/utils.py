e46eed15ce7b67e5c2b9bd2ef35e3a5f94534ea8
Guillaume Vincent <gvincent@redhat.com>
2021-04-23 11:17:52 +0200

    Retry in case of Swift exception
    When we create a file, we may have some 503 error.
    We want to retry 3 times with 1 second delay if an error occurs.
    Add retry in utils module.
    
    Change-Id: Ic3f0139a1d044429cfb428eb6e7eb838266154b0

--- dci/common/utils.py
+++ dci/common/utils.py
@@ -17,7 +17,9 @@
 import datetime
 import hashlib
 import uuid
+import logging
 import flask
+import time
 
 import six
 from sqlalchemy.engine import result
@@ -25,16 +27,17 @@ from werkzeug.routing import BaseConverter, ValidationError
 
 from dci.common import exceptions
 
+logger = logging.getLogger(__name__)
 
-def read(file_path, chunk_size=None, mode='rb'):
-    chunk_size = chunk_size or 1024 ** 2  #  1MB
+
+def read(file_path, chunk_size=None, mode="rb"):
+    chunk_size = chunk_size or 1024 ** 2  # 1MB
     with open(file_path, mode) as f:
         for chunk in iter(lambda: f.read(chunk_size) or None, None):
             yield chunk
 
 
 class UUIDConverter(BaseConverter):
-
     def to_python(self, value):
         try:
             return uuid.UUID(value)
@@ -47,6 +50,7 @@ class UUIDConverter(BaseConverter):
 
 class JSONEncoder(flask.json.JSONEncoder):
     """Default JSON encoder."""
+
     def default(self, o):
         if isinstance(o, datetime.datetime):
             return o.isoformat()
@@ -67,17 +71,34 @@ def gen_etag():
 
     my_salt = gen_uuid()
     if six.PY2:
-        my_salt = my_salt.decode('utf-8')
+        my_salt = my_salt.decode("utf-8")
     elif six.PY3:
-        my_salt = my_salt.encode('utf-8')
+        my_salt = my_salt.encode("utf-8")
     md5 = hashlib.md5()
     md5.update(my_salt)
     return md5.hexdigest()
 
 
 def check_and_get_etag(headers):
-    if_match_etag = headers.get('If-Match')
+    if_match_etag = headers.get("If-Match")
     if not if_match_etag:
-        raise exceptions.DCIException("'If-match' header must be provided",
-                                      status_code=412)
+        raise exceptions.DCIException(
+            "'If-match' header must be provided", status_code=412
+        )
     return if_match_etag
+
+
+def retry(func, *args, **kwargs):
+    tries = kwargs.get("tries", 5)
+    delay = kwargs.get("delay", 1)
+    allowed_exceptions = kwargs.get("allowed_exceptions", ())
+    for _ in range(tries):
+        try:
+            result = func(*args, **kwargs)
+            if result:
+                return result
+        except allowed_exceptions as e:
+            logger.debug("allowed exception in retry function")
+            logger.exception(e)
+        logger.debug("retrying in %d seconds..." % delay)
+        time.sleep(delay)
