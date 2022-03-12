96522ff30d63760b3fd3c3af209066662787f5a6
Guillaume Vincent <gvincent@redhat.com>
2022-03-12 12:16:57 +0000

    Remove models from scripts
    Change-Id: I2ca45e4476105575c33e17d59e01f3f15b091a8a

--- scripts/sync_local_file_with_swift.py
+++ scripts/sync_local_file_with_swift.py
@@ -4,15 +4,15 @@ import os
 import io
 import tqdm
 from dci import dci_config
-from dci.db import models
-from sqlalchemy import sql
+from dci.db import models2
+from sqlalchemy.orm import sessionmaker
 
 
 conf = dci_config.CONFIG
 swift = dci_config.get_store("files")
 engine = dci_config.get_engine(conf).connect()
+session = sessionmaker(bind=engine)()
 
-_TABLE = models.FILES
 
 # Calculate the total files to sync
 file_list = os.walk(conf["FILES_UPLOAD_FOLDER"])
@@ -25,30 +25,26 @@ with tqdm.tqdm(total=sum(1 for _ in file_list)) as pbar:
             continue
         for filename in filenames:
             # Check if file exist in the DB
-            query = sql.select([_TABLE]).where(_TABLE.c.id == filename)
-            result = engine.execute(query)
+            query = session.query(models2.File).filter(models2.File.id == filename)
+            file = query.one()
 
             # If not, do not sync, that's an orphan file
-            if result.rowcount == 0:
+            if file is None:
                 tqdm.tqdm.write("File %s not found, do not sync" % filename)
                 continue
 
             # If the file exist, check if it is already present in swift
             # and then upload it to swift if needed
-            if result.rowcount == 1:
-                tqdm.tqdm.write("File %s found in DB" % filename)
-                folder_len = len(conf["FILES_UPLOAD_FOLDER"])
-                top_path = dirname[folder_len:]
-                row = result.fetchone()
-                swift_path = swift.build_file_path(
-                    row["team_id"], row["job_id"], filename
-                )
-                tqdm.tqdm.write("Check if file is in swift : %s" % swift_path)
-                try:
-                    swift.head(swift_path)
-                    tqdm.tqdm.write("File exist on swift")
-                except:
-                    tqdm.tqdm.write("File not found on swift, we will sync it")
-                    f = io.open(dirname + "/" + filename, "rb")
-                    swift.upload(swift_path, f)
+            tqdm.tqdm.write("File %s found in DB" % filename)
+            folder_len = len(conf["FILES_UPLOAD_FOLDER"])
+            top_path = dirname[folder_len:]
+            swift_path = swift.build_file_path(file.team_id, file.job_id, filename)
+            tqdm.tqdm.write("Check if file is in swift : %s" % swift_path)
+            try:
+                swift.head(swift_path)
+                tqdm.tqdm.write("File exist on swift")
+            except:
+                tqdm.tqdm.write("File not found on swift, we will sync it")
+                f = io.open(dirname + "/" + filename, "rb")
+                swift.upload(swift_path, f)
         pbar.update(1)
