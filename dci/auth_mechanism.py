redhat-cip/dci-control-server

commit 95192bc2a31a7277682e905695072952d2a0763f

Fail auth if parse_headers() returns None
`parse_headers()` can return None if some headers are missing or not
matching a specific format.

If `parse_headers()` returned None, this would generate an unhandled
exception when accessing `headers`. Handing this special case allows to
return an auth error with a more meaningful message.

Change-Id: I50780e52d1e6f9b985af38d25ce19d54b0f40bc8

dci/auth_mechanism.py
@@ -186,6 +186,10 @@ class HmacMechanism(BaseMechanism):
 class Hmac2Mechanism(HmacMechanism):
     def authenticate(self):
         headers = parse_headers(self.request.headers)
+        if not headers:
+            return dci_exc.DCIException(
+                "Hmac2Mechanism failed: bad or incomplete headers.", status_code=400
+            )
         self.identity = self.build_identity(headers)
         if self.identity is None:
             raise dci_exc.DCIException("identity does not exists.", status_code=401)

commit a8748f239579ff697bb84d37ff93a5ecdaf7e2db

Raise exceptions, don't just return them …
Fix regression introduced in Commit 95192bc2

Change-Id: Ieb4b98c8d560ad9e73c670865dc8ca7947345959

dci/auth_mechanism.py
@@ -187,7 +187,7 @@ class Hmac2Mechanism(HmacMechanism):
     def authenticate(self):
         headers = parse_headers(self.request.headers)
         if not headers:
-            return dci_exc.DCIException(
+            raise dci_exc.DCIException(
                 "Hmac2Mechanism failed: bad or incomplete headers.", status_code=400
             )
         self.identity = self.build_identity(headers)