1b9bcb09144a6a18fd98baae663cf5c2f75fc6ce
ylamgarchal <yassine.lamgarchal@redhat.com>
2024-09-26 16:10:05 +0200

    query_es_dsl: allow ":" in list values
    Change-Id: Ib7579d12e3f2a40ccac93a7543df258ee3fc2d17

--- dci/analytics/query_es_dsl.py
+++ dci/analytics/query_es_dsl.py
@@ -18,7 +18,7 @@ import pyparsing as pp
 
 _field = pp.Word(pp.alphanums + "_" + ".")
 _value = pp.Word(pp.alphanums + "_" + "-" + "%" + "." + ":")
-_word = pp.Word(pp.alphanums + "_" + "-" + "." + " ")
+_word = pp.Word(pp.alphanums + "_" + "-" + "." + " " + ":")
 _comma = pp.Suppress(pp.Literal(","))
 _lp = pp.Suppress(pp.Literal("("))
 _rp = pp.Suppress(pp.Literal(")"))
