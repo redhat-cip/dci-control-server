84463ac2d9a723237a4e0fc7f4f61e4e1f2cb8dd
François Charlier <francois.charlier@redhat.com>
2025-05-28 16:50:24 +0200

    Add parameters for gunicorn max_requests & timeout
    This will allow tuning these settings more easily.
    
    Change-Id: Iada60fbbb6f0d2229b3224b4032bed568daa5ad6

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,17 +7,19 @@ worker_connections = int(os.getenv("DCI_GUNICORN_WORKER_CONNECTIONS", 50))
 worker_class = "gevent"
 
 # Restart worker after a certain amount of requests to mitigate memory leaks, etc. …
-max_requests = 1000
+if os.getenv("DCI_GUNICORN_MAX_REQUESTS") is not None:
+    max_requests = int(os.getenv("DCI_GUNICORN_MAX_REQUESTS"))
 # Add some jitter to avoid all workers restarting at the same time.
-max_requests_jitter = 250
+if os.getenv("DCI_GUNICORN_MAX_REQUESTS_JITTER") is not None:
+    max_requests_jitter = int(os.getenv("DCI_GUNICORN_MAX_REQUESTS_JITTER"))
+
+if os.getenv("DCI_GUNICORN_TIMEOUT") is not None:
+    timeout = int(os.getenv("DCI_GUNICORN_TIMEOUT"))
+
+if os.getenv("DCI_GUNICORN_GRACEFUL_TIMEOUT") is not None:
+    graceful_timeout = int(os.getenv("DCI_GUNICORN_GRACEFUL_TIMEOUT"))
 
 accesslog = "-"
 access_log_format = (
     '%(h)s:%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 )
-
-# Some default values from gunicorn that might be of interest to tune
-# max_connections = 1000
-# backlog = 2048
-# timeout = 30
-# graceful_timeout = 30
